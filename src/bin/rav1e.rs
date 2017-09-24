523ef72dcf51492cdaca5c1afef751b0e871bc81
Guillaume Martres <smarter@ubuntu.com>
2017-09-24 16:04:17 +0200

    Add a simple REPL
    Currently only contains one command "process_repl" that encodes one frame.
    
    To run the REPL:
        cargo run --bin rav1repl
    
    To run the main encoder (unfortunately there is no way to make "cargo run" do this by default):
        cargo run --bin rav1e

---
+++ src/bin/rav1e.rs
@@ -0,0 +1,22 @@
+extern crate rav1e;
+extern crate y4m;
+
+use rav1e::*;
+
+fn main() {
+    let mut files = EncoderFiles::from_cli();
+    let mut y4m_dec = y4m::decode(&mut files.input_file).unwrap();
+    let width = y4m_dec.get_width();
+    let height = y4m_dec.get_height();
+    let mut y4m_enc = y4m::encode(width,height,y4m::Ratio::new(30,1)).write_header(&mut files.rec_file).unwrap();
+    let fi = FrameInvariants::new(width, height);
+    let sequence = Sequence::new();
+    write_ivf_header(&mut files.output_file, fi.sb_width*64, fi.sb_height*64);
+
+    let mut frame_number = 0;
+    loop {
+        process_frame(frame_number, &sequence, &fi,
+                      &mut files.output_file, &mut y4m_dec, &mut y4m_enc);
+        frame_number += 1;
+    }
+}
