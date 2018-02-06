3f179a8520d5b6a71807fbcf05f13c7deac32bd5
Thomas Daede <tdaede@xiph.org>
2018-02-06 01:53:37 -0800

    Make rec.y4m optional

--- src/bin/rav1e.rs
+++ src/bin/rav1e.rs
@@ -8,7 +8,10 @@ fn main() {
     let mut y4m_dec = y4m::decode(&mut files.input_file).unwrap();
     let width = y4m_dec.get_width();
     let height = y4m_dec.get_height();
-    let mut y4m_enc = y4m::encode(width,height,y4m::Ratio::new(30,1)).write_header(&mut files.rec_file).unwrap();
+    let mut y4m_enc = match files.rec_file {
+        Some(f) => Some(y4m::encode(width,height,y4m::Ratio::new(30,1)).write_header(&mut f).unwrap()),
+        None => None
+    };
     let fi = FrameInvariants::new(width, height);
     let sequence = Sequence::new();
     write_ivf_header(&mut files.output_file, fi.sb_width*64, fi.sb_height*64);
