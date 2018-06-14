a4be4d8d99f558631b398568cc51cba956d759c1
Frank Bossen <fbossen@gmail.com>
2018-06-14 09:47:52 -0700

    Add rouding to quantization process
    A rounding offset of 21/64 is added to reduce
    reconstruction error

--- src/quantize.rs
+++ src/quantize.rs
@@ -22,12 +22,18 @@ pub fn quantize_in_place(qindex: usize, coeffs: &mut [i32], tx_size: TxSize) {
         TxSize::TX_32X32 => 2,
         _ => 1
     };
-    coeffs[0] *= tx_scale;
-    coeffs[0] /= dc_q(qindex) as i32;
+    let dc_quant = dc_q(qindex) as i32;
     let ac_quant = ac_q(qindex) as i32;
+    // using 21/64=0.328125 as rounding offset. To be tuned
+    let dc_offset = dc_quant * 21 / 64 as i32;
+    let ac_offset = ac_quant * 21 / 64 as i32;
+    coeffs[0] *= tx_scale;
+    coeffs[0] += coeffs[0].signum() * dc_offset;
+    coeffs[0] /= dc_quant;
 
     for c in coeffs[1..].iter_mut() {
         *c *= tx_scale;
+        *c += c.signum() * ac_offset;
         *c /= ac_quant;
     }
     // workaround for bug in token coder
