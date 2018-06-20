6f61991769ad5225fbdbb7e6e5b9dbe98464a582
Raphaël Zumer <rzumer@gmail.com>
2018-06-20 18:22:55 -0400

    Update copyright notice headers
    Update the year and add a header to files where it was missing.

--- src/context.rs
+++ src/context.rs
@@ -1,3 +1,12 @@
+// Copyright (c) 2017-2018, The rav1e contributors. All rights reserved
+//
+// This source code is subject to the terms of the BSD 2 Clause License and
+// the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+// was not distributed with this source code in the LICENSE file, you can
+// obtain it at www.aomedia.org/license/software. If the Alliance for Open
+// Media Patent License 1.0 was not distributed with this source code in the
+// PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+
 #![allow(safe_extern_statics)]
 #![allow(non_upper_case_globals)]
 #![allow(dead_code)]
