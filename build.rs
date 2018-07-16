74fed766ec8eeada633c81a989314039abb4ce58
Michael Bebenita <mbebenita@gmail.com>
2018-07-16 12:35:55 -0400

    Call SSE variants for transforms.

--- build.rs
+++ build.rs
@@ -41,7 +41,6 @@ fn main() {
         .define("CONFIG_ANALYZER", "0")
         .define("CONFIG_Q_ADAPT_PROBS", "0")
         .define("CONFIG_INTRA_EDGE", "0")
-        .define("AOM_TARGET_CPU", "generic")
         .define("ENABLE_DOCS", "0")
         .build();
 
