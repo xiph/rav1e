b15fcfdf8b22ea5015c4dc0c5ee38ae30a164909
Luca Barbato <lu_zero@gentoo.org>
2018-07-21 10:19:30 -0400

    Do not use SIMD for TxType::IDTX ith
    The implementation of it is incomplete.

--- src/transform.rs
+++ src/transform.rs
@@ -111,6 +111,13 @@ extern {
     tx_type: libc::c_int,
     bd: libc::c_int
   );
+  fn av1_inv_txfm2d_add_32x32_c(
+    input: *const i32,
+    output: *mut u16,
+    stride: libc::c_int,
+    tx_type: libc::c_int,
+    bd: libc::c_int
+  );
 }
 
 pub fn forward_transform(
@@ -270,12 +277,23 @@ fn iht32x32_add(
   input: &[i32], output: &mut [u16], stride: usize, tx_type: TxType
 ) {
   unsafe {
-    av1_inv_txfm2d_add_32x32(
-      input.as_ptr(),
-      output.as_mut_ptr(),
-      stride as libc::c_int,
-      tx_type as libc::c_int,
-      8
-    );
+    if tx_type < TxType::IDTX {
+      // SIMDI code may assert for transform types beyond TxType::IDTX.
+      av1_inv_txfm2d_add_32x32(
+        input.as_ptr(),
+        output.as_mut_ptr(),
+        stride as libc::c_int,
+        tx_type as libc::c_int,
+        8
+      );
+    } else {
+      av1_inv_txfm2d_add_32x32_c(
+        input.as_ptr(),
+        output.as_mut_ptr(),
+        stride as libc::c_int,
+        tx_type as libc::c_int,
+        8
+      );
+    }
   }
 }
