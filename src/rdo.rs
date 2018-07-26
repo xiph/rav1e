xiph/rav1e

commit be197cf580d5e45d867d09d75d6bb7b2da2c77c9

Add block skip RDO decision at low speed levels (#280)
* Add block skip RDO decision at low speed levels

* Remove redundant checkpoint instantiations

* Fix duplicate context reset call

src/rdo.rs
@@ -36,7 +36,8 @@ pub struct RDOOutput {
 pub struct RDOPartitionOutput {
     pub rd_cost: f64,
     pub bo: BlockOffset,
-    pub pred_mode: PredictionMode
+    pub pred_mode: PredictionMode,
+    pub skip: bool
 }
 
 // Sum of Squared Error for a wxh block
@@ -98,6 +99,7 @@ fn compute_rd_cost(fi: &FrameInvariants, fs: &FrameState,
 pub fn rdo_mode_decision(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut ContextWriter,
                          bsize: BlockSize, bo: &BlockOffset) -> RDOOutput {
     let mut best_mode = PredictionMode::DC_PRED;
+    let mut best_skip = false;
     let mut best_rd = std::f64::MAX;
     let tell = cw.w.tell_frac();
 
@@ -118,25 +120,33 @@ pub fn rdo_mode_decision(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut Con
     let partition_start_x = (bo.x & LOCAL_BLOCK_MASK) >> xdec << MI_SIZE_LOG2;
     let partition_start_y = (bo.y & LOCAL_BLOCK_MASK) >> ydec << MI_SIZE_LOG2;
 
-    for &mode in RAV1E_INTRA_MODES {
-        if fi.frame_type == FrameType::KEY && mode >= PredictionMode::NEARESTMV {
-            break;
+    for &skip in [false, true].iter() {
+        // Don't test skipped blocks at higher speed levels
+        if fi.speed > 1 && skip == true {
+            continue;
         }
 
         let checkpoint = cw.checkpoint();
 
-        encode_block(fi, fs, cw, mode, bsize, bo);
+        for &mode in RAV1E_INTRA_MODES {
+            if fi.frame_type == FrameType::KEY && mode >= PredictionMode::NEARESTMV {
+                break;
+            }
 
-        let cost = cw.w.tell_frac() - tell;
-        let rd = compute_rd_cost(fi, fs, w, h, w_uv, h_uv,
-                                 partition_start_x, partition_start_y, bo, cost);
+            encode_block(fi, fs, cw, mode, bsize, bo, skip);
 
-        if rd < best_rd {
-            best_rd = rd;
-            best_mode = mode;
+            let cost = cw.w.tell_frac() - tell;
+            let rd = compute_rd_cost(fi, fs, w, h, w_uv, h_uv,
+                                     partition_start_x, partition_start_y, bo, cost);
+
+            if rd < best_rd {
+                best_rd = rd;
+                best_mode = mode;
+                best_skip = skip;
+            }
+
+            cw.rollback(&checkpoint);
         }
-
-        cw.rollback(&checkpoint);
     }
 
     assert!(best_rd >= 0_f64);
@@ -144,7 +154,11 @@ pub fn rdo_mode_decision(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut Con
     let rdo_output = RDOOutput {
         rd_cost: best_rd,
         part_type: PartitionType::PARTITION_NONE,
-        part_modes: vec![RDOPartitionOutput { bo: bo.clone(), pred_mode: best_mode, rd_cost: best_rd }]
+        part_modes: vec![RDOPartitionOutput {
+            bo: bo.clone(),
+            pred_mode: best_mode,
+            rd_cost: best_rd,
+            skip: best_skip }]
     };
 
     rdo_output
@@ -175,13 +189,13 @@ pub fn rdo_tx_type_decision(fi: &FrameInvariants, fs: &mut FrameState,
     let partition_start_x = (bo.x & LOCAL_BLOCK_MASK) >> xdec << MI_SIZE_LOG2;
     let partition_start_y = (bo.y & LOCAL_BLOCK_MASK) >> ydec << MI_SIZE_LOG2;
 
+    let checkpoint = cw.checkpoint();
+
     for &tx_type in RAV1E_TX_TYPES {
         // Skip unsupported transform types
         if av1_ext_tx_used[tx_set_type as usize][tx_type as usize] == 0 {
             continue;
         }
-
-        let checkpoint = cw.checkpoint();
 
         write_tx_blocks(fi, fs, cw, mode, bo, bsize, tx_size, tx_type, false);
 
@@ -212,13 +226,13 @@ pub fn rdo_partition_decision(fi: &FrameInvariants, fs: &mut FrameState,
     let mut best_rd = cached_block.rd_cost;
     let mut best_pred_modes = cached_block.part_modes.clone();
 
+    let checkpoint = cw.checkpoint();
+
     for &partition in RAV1E_PARTITION_TYPES {
         // Do not re-encode results we already have
         if partition == cached_block.part_type && cached_block.rd_cost < max_rd {
             continue;
         }
-
-        let checkpoint = cw.checkpoint();
 
         let mut rd: f64;
         let mut child_modes = std::vec::Vec::new();

src/lib.rs
@@ -514,8 +514,7 @@ pub fn encode_tx_block(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut Conte
 }
 
 fn encode_block(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut ContextWriter,
-            mode: PredictionMode, bsize: BlockSize, bo: &BlockOffset) {
-    let skip = false;
+            mode: PredictionMode, bsize: BlockSize, bo: &BlockOffset, skip: bool) {
     let is_inter = mode >= PredictionMode::NEARESTMV;
 
     cw.bc.set_skip(bo, bsize, skip);
@@ -548,6 +547,10 @@ fn encode_block(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut ContextWrite
         }
     }
 
+    if skip {
+        cw.bc.reset_skip_context(bo, bsize, xdec, ydec);
+    }
+
     // these rules follow TX_MODE_LARGEST
     let tx_size = match bsize {
         BlockSize::BLOCK_4X4 => TxSize::TX_4X4,
@@ -555,10 +558,6 @@ fn encode_block(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut ContextWrite
         BlockSize::BLOCK_16X16 => TxSize::TX_16X16,
         _ => TxSize::TX_32X32
     };
-
-    if skip {
-        cw.bc.reset_skip_context(bo, bsize, xdec, ydec);
-    }
 
     // Luma plane transform type decision
     let tx_set_type = get_ext_tx_set_type(tx_size, is_inter, fi.use_reduced_tx_set);
@@ -670,7 +669,8 @@ bsize: BlockSize, bo: &BlockOffset) -> f64 {
     let mut best_decision = RDOPartitionOutput {
         rd_cost: rd_cost,
         bo: bo.clone(),
-        pred_mode: PredictionMode::DC_PRED
+        pred_mode: PredictionMode::DC_PRED,
+        skip: false
     }; // Best decision that is not PARTITION_SPLIT
 
     let hbs = bs >> 1; // Half the block size in blocks
@@ -688,9 +688,10 @@ bsize: BlockSize, bo: &BlockOffset) -> f64 {
 
         let mode_decision = rdo_mode_decision(fi, fs, cw, bsize, bo).part_modes[0].clone();
         let pred_mode = mode_decision.pred_mode;
+        let skip = mode_decision.skip;
         rd_cost = mode_decision.rd_cost;
 
-        encode_block(fi, fs, cw, pred_mode, bsize, bo);
+        encode_block(fi, fs, cw, pred_mode, bsize, bo, skip);
 
         best_decision = mode_decision;
     }
@@ -725,7 +726,8 @@ bsize: BlockSize, bo: &BlockOffset) -> f64 {
 
             // FIXME: redundant block re-encode
             let pred_mode = best_decision.pred_mode;
-            encode_block(fi, fs, cw, pred_mode, bsize, bo);
+            let skip = best_decision.skip;
+            encode_block(fi, fs, cw, pred_mode, bsize, bo, skip);
         }
     }
 
@@ -785,16 +787,19 @@ fn encode_partition_topdown(fi: &FrameInvariants, fs: &mut FrameState, cw: &mut 
 
     match partition {
         PartitionType::PARTITION_NONE => {
-            let pred_mode = if rdo_output.part_modes.len() > 0 {
+            let part_decision = if rdo_output.part_modes.len() > 0 {
                     // The optimal prediction mode is known from a previous iteration
-                    rdo_output.part_modes[0].pred_mode
+                    rdo_output.part_modes[0].clone()
                 } else {
                     // Make a prediction mode decision for blocks encoded with no rdo_partition_decision call (e.g. edges)
-                    rdo_mode_decision(fi, fs, cw, bsize, bo).part_modes[0].pred_mode
+                    rdo_mode_decision(fi, fs, cw, bsize, bo).part_modes[0].clone()
                 };
 
+            let pred_mode = part_decision.pred_mode;
+            let skip = part_decision.skip;
+
             // FIXME: every final block that has gone through the RDO decision process is encoded twice
-            encode_block(fi, fs, cw, pred_mode, bsize, bo);
+            encode_block(fi, fs, cw, pred_mode, bsize, bo, skip);
         },
         PartitionType::PARTITION_SPLIT => {
             if rdo_output.part_modes.len() >= 4 {

commit 41187872e24c4f9c2edd00f3397d15e4ab127faa

Quick fix the corruption with speed=0 or 1 (#374)
The commit be197cf580d5e45d867d09d75d6bb7b2da2c77c9 has caused
the corrupted decoded video frames with speed=0 or 1, since its landing on
June 26.

The quick fix is to disable skip RDO loop, removing its outermost loop,
and set skip = false.

This bug has been there around a month and believed to be the culprit of
breakage of ext_partition_types PR by TD-Linux as well.

Better fix will come, which is also coincidentally stated earlier in
https://github.com/xiph/rav1e/issues/373.

src/rdo.rs
@@ -190,58 +190,28 @@ pub fn rdo_mode_decision(
   let partition_start_x = (bo.x & LOCAL_BLOCK_MASK) >> xdec << MI_SIZE_LOG2;
   let partition_start_y = (bo.y & LOCAL_BLOCK_MASK) >> ydec << MI_SIZE_LOG2;
 
-  for &skip in &[false, true] {
-    // Don't test skipped blocks at higher speed levels
-    if fi.config.speed > 1 && skip {
-      continue;
+  let skip = false;
+
+  let checkpoint = cw.checkpoint();
+
+  // Exclude complex prediction modes at higher speed levels
+  let mode_set = if fi.config.speed <= 3 {
+    RAV1E_INTRA_MODES
+  } else {
+    RAV1E_INTRA_MODES_MINIMAL
+  };
+
+  for &luma_mode in mode_set {
+    if fi.frame_type == FrameType::KEY
+      && luma_mode >= PredictionMode::NEARESTMV
+    {
+      break;
     }
 
-    let checkpoint = cw.checkpoint();
-
-    // Exclude complex prediction modes at higher speed levels
-    let mode_set = if fi.config.speed <= 3 {
-      RAV1E_INTRA_MODES
-    } else {
-      RAV1E_INTRA_MODES_MINIMAL
-    };
-
-    for &luma_mode in mode_set {
-      if fi.frame_type == FrameType::KEY
-        && luma_mode >= PredictionMode::NEARESTMV
-      {
-        break;
-      }
-
-      if is_chroma_block && fi.config.speed <= 3 {
-        // Find the best chroma prediction mode for the current luma prediction mode
-        for &chroma_mode in RAV1E_INTRA_MODES {
-          encode_block(fi, fs, cw, luma_mode, chroma_mode, bsize, bo, skip);
-
-          let cost = cw.w.tell_frac() - tell;
-          let rd = compute_rd_cost(
-            fi,
-            fs,
-            w,
-            h,
-            w_uv,
-            h_uv,
-            partition_start_x,
-            partition_start_y,
-            bo,
-            cost
-          );
-
-          if rd < best_rd {
-            best_rd = rd;
-            best_mode_luma = luma_mode;
-            best_mode_chroma = chroma_mode;
-            best_skip = skip;
-          }
-
-          cw.rollback(&checkpoint);
-        }
-      } else {
-        encode_block(fi, fs, cw, luma_mode, luma_mode, bsize, bo, skip);
+    if is_chroma_block && fi.config.speed <= 3 {
+      // Find the best chroma prediction mode for the current luma prediction mode
+      for &chroma_mode in RAV1E_INTRA_MODES {
+        encode_block(fi, fs, cw, luma_mode, chroma_mode, bsize, bo, skip);
 
         let cost = cw.w.tell_frac() - tell;
         let rd = compute_rd_cost(
@@ -260,12 +230,37 @@ pub fn rdo_mode_decision(
         if rd < best_rd {
           best_rd = rd;
           best_mode_luma = luma_mode;
-          best_mode_chroma = luma_mode;
+          best_mode_chroma = chroma_mode;
           best_skip = skip;
         }
 
         cw.rollback(&checkpoint);
       }
+    } else {
+      encode_block(fi, fs, cw, luma_mode, luma_mode, bsize, bo, skip);
+
+      let cost = cw.w.tell_frac() - tell;
+      let rd = compute_rd_cost(
+        fi,
+        fs,
+        w,
+        h,
+        w_uv,
+        h_uv,
+        partition_start_x,
+        partition_start_y,
+        bo,
+        cost
+      );
+
+      if rd < best_rd {
+        best_rd = rd;
+        best_mode_luma = luma_mode;
+        best_mode_chroma = luma_mode;
+        best_skip = skip;
+      }
+
+      cw.rollback(&checkpoint);
     }
   }
 