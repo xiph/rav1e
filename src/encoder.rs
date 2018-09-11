1bd94a2d4b782446ae4be8645c46136cc988af1f
David Michael Barr <b@rr-dav.id.au>
2018-09-11 10:02:31 +0900

    Add FrameState::window() to create scratch buffers (#552)
    * Plumb SuperBlockOffset and use superblock dimensions

--- src/encoder.rs
+++ src/encoder.rs
@@ -42,6 +42,16 @@ impl Frame {
             p.pad();
         }
     }
+
+    pub fn window(&self, sbo: &SuperBlockOffset) -> Frame {
+        Frame {
+            planes: [
+                self.planes[0].window(&sbo.plane_offset(&self.planes[0].cfg)),
+                self.planes[1].window(&sbo.plane_offset(&self.planes[1].cfg)),
+                self.planes[2].window(&sbo.plane_offset(&self.planes[2].cfg))
+            ]
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -242,6 +252,15 @@ impl FrameState {
             rec: Frame::new(fi.padded_w, fi.padded_h),
             qc: Default::default(),
             cdfs: CDFContext::new(0),
+        }
+    }
+
+    pub fn window(&self, sbo: &SuperBlockOffset) -> FrameState {
+        FrameState {
+            input: self.input.window(sbo),
+            rec: self.rec.window(sbo),
+            qc: self.qc.clone(),
+            cdfs: self.cdfs.clone()
         }
     }
 }
@@ -1983,3 +2002,35 @@ pub fn update_rec_buffer(fi: &mut FrameInvariants, fs: FrameState) {
   }
 }
 
+#[cfg(test)]
+mod test {
+  #[test]
+  fn frame_state_window() {
+    use super::*;
+    let config = EncoderConfig { ..Default::default() };
+    let fi = FrameInvariants::new(1024, 1024, config);
+    let mut fs = FrameState::new(&fi);
+    for p in fs.rec.planes.iter_mut() {
+      for (i, v) in p
+        .mut_slice(&PlaneOffset { x: 0, y: 0 })
+        .as_mut_slice()
+        .iter_mut()
+        .enumerate()
+      {
+        *v = i as u16;
+      }
+    }
+    let offset = BlockOffset { x: 56, y: 56 };
+    let sbo = offset.sb_offset();
+    let fs_ = fs.window(&sbo);
+    for p in 0..3 {
+      assert!(fs_.rec.planes[p].cfg.xorigin < 0);
+      assert!(fs_.rec.planes[p].cfg.yorigin < 0);
+      let po = offset.plane_offset(&fs.rec.planes[p].cfg);
+      assert_eq!(
+        fs.rec.planes[p].slice(&po).as_slice()[..32],
+        fs_.rec.planes[p].slice(&po).as_slice()[..32]
+      );
+    }
+  }
+}
