72e1fac3e8674b4f8efc9025e3faaa9f954fbd6f
Monty Montgomery <xiphmont@gmail.com>
2018-10-24 23:21:42 -0400

    Add RestorationContext infrastructure for loop restoration filter

--- benches/bench.rs
+++ benches/bench.rs
@@ -51,7 +51,9 @@ fn write_b_bench(b: &mut Bencher, tx_size: TxSize, qindex: usize) {
   let fc = CDFContext::new(fi.base_q_idx);
   let bc = BlockContext::new(fi.sb_width * 16, fi.sb_height * 16);
   let mut fs = FrameState::new(&fi);
-  let mut cw = ContextWriter::new(fc, bc);
+  // For now, restoration unit size is locked to superblock size. 
+  let rc = RestorationContext::new(fi.sb_width, fi.sb_height);
+  let mut cw = ContextWriter::new(fc, bc, rc);
 
   let tx_type = TxType::DCT_DCT;
 
