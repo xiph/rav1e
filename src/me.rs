819502954a61f9e397a81d79b768cc2dfd58300b
Kyle Siefring <kylesiefring@gmail.com>
2018-11-03 17:00:51 -0400

    Use all 4x4, 8x8, 16x16, and 32x32 sad asm.

--- src/me.rs
+++ src/me.rs
@@ -14,6 +14,7 @@ use partition::*;
 use plane::*;
 use FrameInvariants;
 use FrameState;
+use util::*;
 
 use libc;
 
@@ -47,13 +48,27 @@ unsafe fn sad_ssse3(plane_org: &PlaneSlice, plane_ref: &PlaneSlice, blk_h: usize
   // TODO: stride *2??? What is the correct way to do this?
   let org_stride = plane_org.plane.cfg.stride as libc::ptrdiff_t * 2;
   let ref_stride = plane_ref.plane.cfg.stride as libc::ptrdiff_t * 2;
-  for r in (0..blk_h).step_by(32) {
-    for c in (0..blk_w).step_by(32) {
+  assert!(blk_h >= 4 && blk_w >= 4);
+  let step_size = blk_h.min(blk_w).min(32);
+  let func = match step_size.ilog() {
+    3 => rav1e_sad_4x4_ssse3,
+    4 => rav1e_sad_8x8_ssse3,
+    5 => rav1e_sad_16x16_ssse3,
+    6 => rav1e_sad_32x32_ssse3,
+    _ => rav1e_sad_32x32_ssse3
+  };
+  if blk_h == blk_w && blk_h <= 32 {
+    let org_ptr = plane_org.as_slice().as_ptr();
+    let ref_ptr = plane_ref.as_slice().as_ptr();
+    return func(org_ptr, org_stride, ref_ptr, ref_stride);
+  }
+  for r in (0..blk_h).step_by(step_size) {
+    for c in (0..blk_w).step_by(step_size) {
       let org_slice = plane_org.subslice(c, r);
       let ref_slice = plane_ref.subslice(c, r);
       let org_ptr = org_slice.as_slice().as_ptr();
       let ref_ptr = ref_slice.as_slice().as_ptr();
-      sum += rav1e_sad_32x32_ssse3(org_ptr, org_stride, ref_ptr, ref_stride);
+      sum += func(org_ptr, org_stride, ref_ptr, ref_stride);
     }
   }
   return sum;
@@ -66,7 +81,7 @@ pub fn get_sad(
 ) -> u32 {
   #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
   {
-    if is_x86_feature_detected!("ssse3") && blk_h >= 32 && blk_w >= 32 {
+    if is_x86_feature_detected!("ssse3") && blk_h >= 4 && blk_w >= 4 {
       return unsafe { sad_ssse3(plane_org, plane_ref, blk_h, blk_w) };
     }
   }
--- src/x86/me.asm
+++ src/x86/me.asm
@@ -4,6 +4,17 @@
 
 SECTION .text
 
+%macro W_ABS_DIFF 8
+    psubw               %1, %5
+    psubw               %2, %6
+    psubw               %3, %7
+    psubw               %4, %8
+    pabsw               %1, %1
+    pabsw               %2, %2
+    pabsw               %3, %3
+    pabsw               %4, %4
+%endmacro
+
 INIT_XMM ssse3
 cglobal sad_4x4, 4, 6, 8, src, src_stride, dst, dst_stride, \
                           src_stride3, dst_stride3
@@ -17,14 +28,7 @@ cglobal sad_4x4, 4, 6, 8, src, src_stride, dst, dst_stride, \
     movq                m5, [dstq+dst_strideq*1]
     movq                m6, [dstq+dst_strideq*2]
     movq                m7, [dstq+dst_stride3q]
-    psubw               m0, m4
-    psubw               m1, m5
-    psubw               m2, m6
-    psubw               m3, m7
-    pabsw               m0, m0
-    pabsw               m1, m1
-    pabsw               m2, m2
-    pabsw               m3, m3
+    W_ABS_DIFF m0, m1, m2, m3, m4, m5, m6, m7 
     paddw               m0, m1
     paddw               m2, m3
     paddw               m0, m2
@@ -46,7 +50,8 @@ cglobal sad_8x8, 4, 7, 9, src, src_stride, dst, dst_stride, \
     lea       src_stride3q, [src_strideq*3]
     lea       dst_stride3q, [dst_strideq*3]
     mov               cntd, 2
-    pxor                m0, m0
+    %define            sum  m0
+    pxor               sum, sum
 .loop:
     movu                m1, [srcq]
     movu                m2, [srcq+src_strideq*1]
@@ -58,26 +63,19 @@ cglobal sad_8x8, 4, 7, 9, src, src_stride, dst, dst_stride, \
     movu                m7, [dstq+dst_strideq*2]
     movu                m8, [dstq+dst_stride3q]
     lea               dstq, [dstq+dst_strideq*4]
-    psubw               m1, m5
-    psubw               m2, m6
-    psubw               m3, m7
-    psubw               m4, m8
-    pabsw               m1, m1
-    pabsw               m2, m2
-    pabsw               m3, m3
-    pabsw               m4, m4
+    W_ABS_DIFF m1, m2, m3, m4, m5, m6, m7, m8 
     paddw               m1, m2
     paddw               m3, m4
-    paddw               m0, m1
-    paddw               m0, m3
+    paddw              sum, m1
+    paddw              sum, m3
     dec               cntd
     jg .loop
-    movhlps             m1, m0
-    paddw               m0, m1
-    pshuflw             m1, m0, q2323
-    paddw               m0, m1
-    pshuflw             m1, m0, q1111
-    paddw               m0, m1
+    movhlps             m1, sum
+    paddw              sum, m1
+    pshuflw             m1, sum, q2323
+    paddw              sum, m1
+    pshuflw             m1, sum, q1111
+    paddw              sum, m1
     movd               eax, m0
     movzx              eax, ax
     RET
@@ -86,7 +84,8 @@ INIT_XMM ssse3
 cglobal sad_16x16, 4, 5, 9, src, src_stride, dst, dst_stride, \
                             cnt
     mov               cntd, 8
-    pxor                m0, m0
+    %define            sum  m0
+    pxor               sum, sum
 .loop:
     movu                m1, [srcq]
     movu                m2, [srcq+16]
@@ -98,73 +97,59 @@ cglobal sad_16x16, 4, 5, 9, src, src_stride, dst, dst_stride, \
     movu                m7, [dstq+dst_strideq]
     movu                m8, [dstq+dst_strideq+16]
     lea               dstq, [dstq+dst_strideq*2]
-    psubw               m1, m5
-    psubw               m2, m6
-    psubw               m3, m7
-    psubw               m4, m8
-    pabsw               m1, m1
-    pabsw               m2, m2
-    pabsw               m3, m3
-    pabsw               m4, m4
+    W_ABS_DIFF m1, m2, m3, m4, m5, m6, m7, m8 
     paddw               m1, m2
     paddw               m3, m4
-    paddw               m0, m1
-    paddw               m0, m3
+    paddw              sum, m1
+    paddw              sum, m3
     dec               cntd
     jg .loop
 ; convert to 32-bit
     pxor                m1, m1
-    punpcklwd           m2, m0, m1
-    punpckhwd           m0, m1
-    paddd               m0, m2
-    movhlps             m1, m0
-    paddd               m0, m1
-    pshufd              m1, m0, q1111
-    paddd               m0, m1
-    movd               eax, m0
+    punpcklwd           m2, sum, m1
+    punpckhwd          sum, m1
+    paddd              sum, m2
+    movhlps             m1, sum
+    paddd              sum, m1
+    pshufd              m1, sum, q1111
+    paddd              sum, m1
+    movd               eax, sum
     RET
 
 ;10 bit only
 INIT_XMM ssse3
 cglobal sad_32x32, 4, 5, 10, src, src_stride, dst, dst_stride, \
-                            cnt
+                             cnt
     mov               cntd, 32
     pxor                m0, m0
-    pxor                m9, m9
+    pxor                m1, m1
 .loop:
-    movu                m1, [srcq]
-    movu                m2, [srcq+16]
-    movu                m3, [srcq+32]
-    movu                m4, [srcq+48]
+    movu                m2, [srcq]
+    movu                m3, [srcq+16]
+    movu                m4, [srcq+32]
+    movu                m5, [srcq+48]
     lea               srcq, [srcq+src_strideq]
-    movu                m5, [dstq]
-    movu                m6, [dstq+16]
-    movu                m7, [dstq+32]
-    movu                m8, [dstq+48]
+    movu                m6, [dstq]
+    movu                m7, [dstq+16]
+    movu                m8, [dstq+32]
+    movu                m9, [dstq+48]
     lea               dstq, [dstq+dst_strideq]
-    psubw               m1, m5
-    psubw               m2, m6
-    psubw               m3, m7
-    psubw               m4, m8
-    pabsw               m1, m1
-    pabsw               m2, m2
-    pabsw               m3, m3
-    pabsw               m4, m4
-    paddw               m1, m2
-    paddw               m3, m4
-    paddw               m0, m1
-    paddw               m9, m3
+    W_ABS_DIFF m2, m3, m4, m5, m6, m7, m8, m9 
+    paddw               m2, m3
+    paddw               m4, m5
+    paddw               m0, m2
+    paddw               m1, m4
     dec               cntd
     jg .loop
 ; convert to 32-bit
-    pxor                m1, m1
-    punpcklwd           m2, m0, m1
-    punpckhwd           m0, m1
-    paddd               m0, m2
-    punpcklwd           m3, m9, m1
-    punpckhwd           m9, m1
-    paddd               m9, m3
-    paddd               m0, m9
+    pxor                m2, m2
+    punpcklwd           m3, m0, m2
+    punpckhwd           m0, m2
+    paddd               m0, m3
+    punpcklwd           m3, m1, m2
+    punpckhwd           m1, m2
+    paddd               m1, m3
+    paddd               m0, m1
     movhlps             m1, m0
     paddd               m0, m1
     pshufd              m1, m0, q1111
