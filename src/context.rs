47a27e9816133ace1a3cd6293859ebf6b416424d
Raphaël Zumer <rzumer@tebako.net>
2018-11-15 21:24:09 -0500

    Add missing license headers

--- src/context.rs
+++ src/context.rs
@@ -1,5 +1,5 @@
-//
 // Copyright (c) 2017-2018, The rav1e contributors. All rights reserved
+//
 // This source code is subject to the terms of the BSD 2 Clause License and
 // the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
 // was not distributed with this source code in the LICENSE file, you can
