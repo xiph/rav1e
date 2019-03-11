2659edfbde7c63a6108d93aa710866f6c944a7b5
David Michael Barr <b@rr-dav.id.au>
2019-03-11 19:26:36 +0900

    Add speed setting for CDEF

--- src/encoder.rs
+++ src/encoder.rs
@@ -310,7 +310,7 @@ impl Sequence {
       enable_ref_frame_mvs: false,
       enable_warped_motion: false,
       enable_superres: false,
-      enable_cdef: true,
+      enable_cdef: config.speed_settings.cdef,
       enable_restoration: config.chroma_sampling != ChromaSampling::Cs422 &&
         config.chroma_sampling != ChromaSampling::Cs444, // FIXME: not working yet
       operating_points_cnt_minus_1: 0,
