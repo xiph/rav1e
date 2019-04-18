85cd30c174a276bb04ad9c4ab12b1dfabe1e2dc1
Romain Vimont <rom1v@videolabs.io>
2019-04-18 19:31:20 +0200

    Pass MI width and height to get_sub_partitions()
    The function get_sub_partitions_with_border_check() retrieved w_in_b and
    h_in_b from FrameInvariants.
    
    Pass these values directly. That will make it usable with tiles.

--- src/rdo.rs
+++ src/rdo.rs
@@ -1063,10 +1063,11 @@ pub fn get_sub_partitions(four_partitions: &[BlockOffset; 4],
   partitions
 }
 
-pub fn get_sub_partitions_with_border_check<T: Pixel>(
+pub fn get_sub_partitions_with_border_check(
   four_partitions: &[BlockOffset; 4],
   partition: PartitionType,
-  fi: &FrameInvariants<T>,
+  mi_width: usize,
+  mi_height: usize,
   subsize: BlockSize
 ) -> Vec<BlockOffset> {
   let mut partitions = vec![ four_partitions[0] ];
@@ -1079,20 +1080,20 @@ pub fn get_sub_partitions_with_border_check<T: Pixel>(
   let hbsh = subsize.height_mi(); // Half the block size height in blocks
 
   if (partition == PARTITION_VERT || partition == PARTITION_SPLIT) &&
-    four_partitions[1].x + hbsw <= fi.w_in_b &&
-    four_partitions[1].y + hbsh <= fi.h_in_b {
+    four_partitions[1].x + hbsw <= mi_width &&
+    four_partitions[1].y + hbsh <= mi_height {
     partitions.push(four_partitions[1]);
   };
 
   if (partition == PARTITION_HORZ || partition == PARTITION_SPLIT) &&
-    four_partitions[2].x + hbsw <= fi.w_in_b &&
-    four_partitions[2].y + hbsh <= fi.h_in_b {
+    four_partitions[2].x + hbsw <= mi_width &&
+    four_partitions[2].y + hbsh <= mi_height {
     partitions.push(four_partitions[2]);
   };
 
   if partition == PARTITION_SPLIT &&
-    four_partitions[3].x + hbsw <= fi.w_in_b &&
-    four_partitions[3].y + hbsh <= fi.h_in_b {
+    four_partitions[3].x + hbsw <= mi_width &&
+    four_partitions[3].y + hbsh <= mi_height {
     partitions.push(four_partitions[3]);
   };
 
@@ -1162,7 +1163,7 @@ pub fn rdo_partition_decision<T: Pixel>(
           BlockOffset{ x: bo.x, y: bo.y + hbsh as usize },
           BlockOffset{ x: bo.x + hbsw as usize, y: bo.y + hbsh as usize }
         ];
-        let partitions = get_sub_partitions_with_border_check(&four_partitions, partition, fi, subsize);
+        let partitions = get_sub_partitions_with_border_check(&four_partitions, partition, fi.w_in_b, fi.h_in_b, subsize);
 
         let pmv_idxs = partitions.iter().map(|&offset| {
           if subsize.greater_than(BlockSize::BLOCK_32X32) {
