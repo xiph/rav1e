fa991a376a96e80889cc29e56d2f9d4a341fd3ed
Xu Guangxin <guangxin.xu@intel.com>
2019-05-06 12:01:07 +0800

    refact: add Muxer trait for encode output

---
+++ src/bin/muxer/mod.rs
@@ -0,0 +1,31 @@
+// Copyright (c) 2017-2019, The rav1e contributors. All rights reserved
+//
+// This source code is subject to the terms of the BSD 2 Clause License and
+// the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+// was not distributed with this source code in the LICENSE file, you can
+// obtain it at www.aomedia.org/license/software. If the Alliance for Open
+// Media Patent License 1.0 was not distributed with this source code in the
+// PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+
+mod muxer;
+pub use muxer::*;
+
+mod ivf;
+use self::ivf::IvfMuxer;
+
+use std::io;
+
+pub trait Muxer {
+  fn write_header(
+    &mut self, width: usize, height: usize, framerate_num: usize,
+    framerate_den: usize
+  );
+
+  fn write_frame(&mut self, pts: u64, data: &[u8]);
+
+  fn flush(&mut self) -> io::Result<()>;
+}
+
+pub fn create_muxer(path: &str) -> Box<dyn Muxer> {
+  IvfMuxer::open(path)
+}
