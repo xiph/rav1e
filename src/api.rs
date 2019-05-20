8a52e459f09bdca1cc3df8a7655cd59be59e19fb
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2019-05-20 22:00:59 +0100

    api: Invert timebase when passing it as a framerate to RCState::new
    It's still sketchy, but not 100% broken, now.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- src/api.rs
+++ src/api.rs
@@ -673,8 +673,8 @@ impl<T: Pixel> ContextInner<T> {
         rc_state: RCState::new(
           enc.width as i32,
           enc.height as i32,
-          enc.time_base.num as i64,
           enc.time_base.den as i64,
+          enc.time_base.num as i64,
           enc.bitrate,
           maybe_ac_qi_max,
           enc.max_key_frame_interval as i32
