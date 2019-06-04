109732039565b554996b80ae5734c4e2a487087a
Luca Barbato <lu_zero@gentoo.org>
2019-06-04 10:20:23 +0000

    Deny bare trait objects

--- src/bin/rav1e.rs
+++ src/bin/rav1e.rs
@@ -7,6 +7,8 @@
 // Media Patent License 1.0 was not distributed with this source code in the
 // PATENTS file, you can obtain it at www.aomedia.org/license/patent.
 
+#![deny(bare_trait_objects)]
+
 #[macro_use]
 extern crate scan_fmt;
 
