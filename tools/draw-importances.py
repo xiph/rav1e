473b71925e06952aa31065bb0806e53f3582d897
Ivan Molodetskikh <yalterz@gmail.com>
2019-07-18 13:13:57 -0700

    Add final block importance computation step

---
+++ tools/draw-importances.py
@@ -0,0 +1,49 @@
+import struct
+import sys
+from os.path import splitext
+import numpy as np
+from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
+
+
+# Renders block importances output by ContextInner::compute_lookahead_data().
+# Usage: draw-importances.py <i-hres.png> <i-imps.bin>
+#        will output i-imps.png.
+
+
+with open(sys.argv[2], 'rb') as f:
+    contents = f.read()
+
+rows, cols = struct.unpack('qq', contents[:16])
+imps = np.frombuffer(contents[16:], dtype=np.float32).reshape((rows, cols))
+max_imp = np.max(imps)
+
+frame_size_multiplier = 4
+
+frame = Image.open(sys.argv[1])
+frame = frame.resize((frame.width * frame_size_multiplier, frame.height * frame_size_multiplier))
+frame = frame.convert(mode='RGB')
+
+mv_original_block_size = 4 // 2 # The MVs are in 4×4 blocks, but we use half-resolution images.
+mv_block_size = mv_original_block_size * frame_size_multiplier
+
+draw = ImageDraw.Draw(frame, mode='RGBA')
+
+# Draw the grid.
+for i in range(0, frame.width, mv_block_size):
+    draw.line(((i, 0), (i, frame.height)), fill=(0, 0, 0, 255))
+for i in range(0, frame.height, mv_block_size):
+    draw.line(((0, i), (frame.width, i)), fill=(0, 0, 0, 255))
+
+# Draw the importances.
+if max_imp > 0:
+    for y in range(rows):
+        for x in range(cols):
+            imp = imps[y, x]
+            top_left = (x * mv_block_size, y * mv_block_size)
+            bottom_right = (top_left[0] + mv_block_size, top_left[1] + mv_block_size)
+            draw.rectangle((top_left, bottom_right), fill=(int(imp / max_imp * 255), 0, 0, 128))
+
+fig = plt.figure(figsize=(frame.width, frame.height), dpi=1)
+fig.figimage(frame, cmap='gray')
+plt.savefig(splitext(sys.argv[2])[0] + '.png', bbox_inches='tight')
