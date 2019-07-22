5390642138caec519137bb9f00d994ba52a2f5e2
Thomas Daede <daede003@umn.edu>
2019-07-21 18:14:04 -0700

    Cleanup luma_chroma_mode_rdo.

--- src/rdo.rs
+++ src/rdo.rs
@@ -465,97 +465,97 @@ fn luma_chroma_mode_rdo<T: Pixel> (luma_mode: PredictionMode,
   luma_mode_is_intra: bool,
   mode_context: usize,
   mv_stack: &ArrayVec<[CandidateMV; 9]>) {
-    let (tx_size, mut tx_type) = rdo_tx_size_type(
-      fi, ts, cw, bsize, tile_bo, luma_mode, ref_frames, mvs, false,
-    );
+  let (tx_size, mut tx_type) = rdo_tx_size_type(
+    fi, ts, cw, bsize, tile_bo, luma_mode, ref_frames, mvs, false,
+  );
 
-    let PlaneConfig { xdec, ydec, .. } = ts.input.planes[1].cfg;
+  let PlaneConfig { xdec, ydec, .. } = ts.input.planes[1].cfg;
 
-    let is_chroma_block = has_chroma(tile_bo, bsize, xdec, ydec);
-    
-    // Find the best chroma prediction mode for the current luma prediction mode
-    let mut chroma_rdo = |skip: bool| {
-      mode_set_chroma.iter().for_each(|&chroma_mode| {
-        let wr = &mut WriterCounter::new();
-        let tell = wr.tell_frac();
+  let is_chroma_block = has_chroma(tile_bo, bsize, xdec, ydec);
 
-        if skip { tx_type = TxType::DCT_DCT; };
+  // Find the best chroma prediction mode for the current luma prediction mode
+  let mut chroma_rdo = |skip: bool| {
+    for &chroma_mode in mode_set_chroma.iter() {
+      let wr = &mut WriterCounter::new();
+      let tell = wr.tell_frac();
 
-        if bsize >= BlockSize::BLOCK_8X8 && bsize.is_sqr() {
-          cw.write_partition(wr, tile_bo, PartitionType::PARTITION_NONE, bsize);
-        }
+      if skip { tx_type = TxType::DCT_DCT; };
 
-        // TODO(yushin): luma and chroma would have different decision based on chroma format
-        let need_recon_pixel = luma_mode_is_intra && tx_size.block_size() != bsize;
+      if bsize >= BlockSize::BLOCK_8X8 && bsize.is_sqr() {
+        cw.write_partition(wr, tile_bo, PartitionType::PARTITION_NONE, bsize);
+      }
 
-        encode_block_pre_cdef(&fi.sequence, ts, cw, wr, bsize, tile_bo, skip);
-        let tx_dist =
-          encode_block_post_cdef(
-            fi,
-            ts,
-            cw,
-            wr,
-            luma_mode,
-            chroma_mode,
-            ref_frames,
-            mvs,
-            bsize,
-            tile_bo,
-            skip,
-            CFLParams::default(),
-            tx_size,
-            tx_type,
-            mode_context,
-            &mv_stack,
-            rdo_type,
-            need_recon_pixel
-          );
+      // TODO(yushin): luma and chroma would have different decision based on chroma format
+      let need_recon_pixel = luma_mode_is_intra && tx_size.block_size() != bsize;
 
-        let rate = wr.tell_frac() - tell;
-        let distortion = if fi.use_tx_domain_distortion && !need_recon_pixel {
-          compute_tx_distortion(
-            fi,
-            ts,
-            bsize,
-            is_chroma_block,
-            tile_bo,
-            tx_dist,
-            skip,
-            false
-          )
-        } else {
-          compute_distortion(
-            fi,
-            ts,
-            bsize,
-            is_chroma_block,
-            tile_bo,
-            false
-          )
-        };
-        let rd = compute_rd_cost(fi, rate, distortion);
-        if rd < best.rd {
-          //if rd < best.rd || luma_mode == PredictionMode::NEW_NEWMV {
-          best.rd = rd;
-          best.mode_luma = luma_mode;
-          best.mode_chroma = chroma_mode;
-          best.ref_frames = ref_frames;
-          best.mvs = mvs;
-          best.skip = skip;
-          best.tx_size = tx_size;
-          best.tx_type = tx_type;
-        }
+      encode_block_pre_cdef(&fi.sequence, ts, cw, wr, bsize, tile_bo, skip);
+      let tx_dist =
+        encode_block_post_cdef(
+          fi,
+          ts,
+          cw,
+          wr,
+          luma_mode,
+          chroma_mode,
+          ref_frames,
+          mvs,
+          bsize,
+          tile_bo,
+          skip,
+          CFLParams::default(),
+          tx_size,
+          tx_type,
+          mode_context,
+          &mv_stack,
+          rdo_type,
+          need_recon_pixel
+        );
 
-        cw.rollback(cw_checkpoint);
-      });
-    };
+      let rate = wr.tell_frac() - tell;
+      let distortion = if fi.use_tx_domain_distortion && !need_recon_pixel {
+        compute_tx_distortion(
+          fi,
+          ts,
+          bsize,
+          is_chroma_block,
+          tile_bo,
+          tx_dist,
+          skip,
+          false
+        )
+      } else {
+        compute_distortion(
+          fi,
+          ts,
+          bsize,
+          is_chroma_block,
+          tile_bo,
+          false
+        )
+      };
+      let rd = compute_rd_cost(fi, rate, distortion);
+      if rd < best.rd {
+        //if rd < best.rd || luma_mode == PredictionMode::NEW_NEWMV {
+        best.rd = rd;
+        best.mode_luma = luma_mode;
+        best.mode_chroma = chroma_mode;
+        best.ref_frames = ref_frames;
+        best.mvs = mvs;
+        best.skip = skip;
+        best.tx_size = tx_size;
+        best.tx_type = tx_type;
+      }
 
-    chroma_rdo(false);
-    // Don't skip when using intra modes
-    if !luma_mode_is_intra {
-      chroma_rdo(true);
-    };
-  }
+      cw.rollback(cw_checkpoint);
+    }
+  };
+
+  chroma_rdo(false);
+  // Don't skip when using intra modes
+  if !luma_mode_is_intra {
+    chroma_rdo(true);
+  };
+}
 
 // RDO-based mode decision
 pub fn rdo_mode_decision<T: Pixel>(
