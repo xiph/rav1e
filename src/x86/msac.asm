478a84f5e712e68e8112623c4e20e282651baa5a
Henrik Gramner <gramner@twoorioles.com>
2019-08-06 15:17:31 +0200

    Add msac optimizations
     * Eliminate the trailing zero after the CDF probabilities. We can
       reuse the count value as a terminator instead. This reduces the
       size of the CDF context by around 8%.
    
     * Align the CDF arrays.
    
     * Various other minor optimizations.

--- src/x86/msac.asm
+++ src/x86/msac.asm
@@ -88,7 +88,7 @@ cglobal msac_decode_symbol_adapt4, 0, 6, 6
     movp           m3, [t0+msac.dif]
     mov           t3d, [t0+msac.update_cdf]
     mov           t4d, t2d
-    neg            t2
+    not            t2     ; -(n_symbols + 1)
     pshuflw        m2, m2, q0000
     movd     [buf+12], m2
     pand           m2, [rax]
@@ -112,15 +112,15 @@ cglobal msac_decode_symbol_adapt4, 0, 6, 6
     pcmpeqw        m2, m2
     mov           t2d, t3d
     shr           t3d, 4
-    cmp           t4d, 4
-    sbb           t3d, -5 ; (count >> 4) + (n_symbols > 3) + 4
+    cmp           t4d, 3
+    sbb           t3d, -5 ; (count >> 4) + (n_symbols > 2) + 4
     cmp           t2d, 32
     adc           t2d, 0  ; count + (count < 32)
     movd           m3, t3d
     pavgw          m2, m1 ; i >= val ? -1 : 32768
     psubw          m2, m0 ; for (i = 0; i < val; i++)
     psubw          m0, m1 ;     cdf[i] += (32768 - cdf[i]) >> rate;
-    psraw          m2, m3 ; for (; i < n_symbols - 1; i++)
+    psraw          m2, m3 ; for (; i < n_symbols; i++)
     paddw          m0, m2 ;     cdf[i] += ((  -1 - cdf[i]) >> rate) + 1;
     movq         [t1], m0
     mov     [t1+t4*2], t2w
@@ -214,11 +214,11 @@ cglobal msac_decode_symbol_adapt8, 0, 6, 6
     DECODE_SYMBOL_ADAPT_INIT
     LEA           rax, pw_0xff00
     movd           m2, [t0+msac.rng]
-    movu           m1, [t1]
+    mova           m1, [t1]
     movp           m3, [t0+msac.dif]
     mov           t3d, [t0+msac.update_cdf]
     mov           t4d, t2d
-    neg            t2
+    not            t2
     pshuflw        m2, m2, q0000
     movd     [buf+12], m2
     punpcklqdq     m2, m2
@@ -242,7 +242,7 @@ cglobal msac_decode_symbol_adapt8, 0, 6, 6
     pcmpeqw        m2, m2
     mov           t2d, t3d
     shr           t3d, 4
-    cmp           t4d, 4 ; may be called with n_symbols < 4
+    cmp           t4d, 3 ; may be called with n_symbols <= 2
     sbb           t3d, -5
     cmp           t2d, 32
     adc           t2d, 0
@@ -252,7 +252,7 @@ cglobal msac_decode_symbol_adapt8, 0, 6, 6
     psubw          m0, m1
     psraw          m2, m3
     paddw          m0, m2
-    movu         [t1], m0
+    mova         [t1], m0
     mov     [t1+t4*2], t2w
     jmp m(msac_decode_symbol_adapt4).renorm
 
@@ -260,12 +260,12 @@ cglobal msac_decode_symbol_adapt16, 0, 6, 6
     DECODE_SYMBOL_ADAPT_INIT
     LEA           rax, pw_0xff00
     movd           m4, [t0+msac.rng]
-    movu           m2, [t1]
-    movu           m3, [t1+16]
+    mova           m2, [t1]
+    mova           m3, [t1+16]
     movp           m5, [t0+msac.dif]
     mov           t3d, [t0+msac.update_cdf]
     mov           t4d, t2d
-    neg            t2
+    not            t2
 %if WIN64
     sub           rsp, 48 ; need 36 bytes, shadow space is only 32
 %endif
@@ -288,8 +288,8 @@ cglobal msac_decode_symbol_adapt16, 0, 6, 6
     punpcklqdq     m5, m5
     paddw          m3, m4
     mova        [buf], m2
-    mova     [buf+16], m3
     psubusw        m2, m5
+    mova     [buf+16], m3
     psubusw        m3, m5
     pxor           m4, m4
     pcmpeqw        m2, m4
@@ -301,7 +301,7 @@ cglobal msac_decode_symbol_adapt16, 0, 6, 6
     movzx         t3d, word [t1+t4*2]
     pcmpeqw        m4, m4
     mova           m5, m4
-    lea           t2d, [t3+80] ; only support n_symbols >= 4
+    lea           t2d, [t3+80] ; only support n_symbols > 2
     shr           t2d, 4
     cmp           t3d, 32
     adc           t3d, 0
@@ -316,8 +316,8 @@ cglobal msac_decode_symbol_adapt16, 0, 6, 6
     psraw          m5, m2
     paddw          m0, m4
     paddw          m1, m5
-    movu         [t1], m0
-    movu      [t1+16], m1
+    mova         [t1], m0
+    mova      [t1+16], m1
     mov     [t1+t4*2], t3w
 .renorm:
     tzcnt         eax, eax
