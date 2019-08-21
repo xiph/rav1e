096ff44b0b225ae3d929c076e2786ae9665cf635
Fredrik Pihl <pi.arctan@gmail.com>
2019-08-21 17:56:33 +0200

    Added Copyright messages.

--- src/rdo_tables.rs
+++ src/rdo_tables.rs
@@ -1,3 +1,13 @@
+// Copyright (c) 2001-2016, Alliance for Open Media. All rights reserved
+// Copyright (c) 2017-2018, The rav1e contributors. All rights reserved
+//
+// This source code is subject to the terms of the BSD 2 Clause License and
+// the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+// was not distributed with this source code in the LICENSE file, you can
+// obtain it at www.aomedia.org/license/software. If the Alliance for Open
+// Media Patent License 1.0 was not distributed with this source code in the
+// PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+
 pub const RDO_NUM_BINS: usize = 50;
 #[allow(unused)]
 pub const RDO_MAX_BIN: usize = 10000;
