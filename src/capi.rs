82158aaf1c3dce3a9c8ebe82b89453a1ae779e5c
Ivan Molodetskikh <yalterz@gmail.com>
2019-08-27 15:59:58 -0700

    capi: fix EncoderStatus docs

--- src/capi.rs
+++ src/capi.rs
@@ -57,28 +57,35 @@ pub struct Frame {
   frame_type: FrameTypeOverride,
 }
 
+/// Status that can be returned by encoder functions.
 #[repr(C)]
 #[derive(Copy, Clone, Debug, FromPrimitive)]
 pub enum EncoderStatus {
-  /// Normal operation
+  /// Normal operation.
   Success = 0,
-  /// The encoder needs more data to produce an output Packet
-  /// May be emitted by `Context::receive_packet`  when frame reordering is enabled.
+  /// The encoder needs more data to produce an output packet.
+  ///
+  /// May be emitted by `rav1e_receive_packet` when frame reordering is
+  /// enabled.
   NeedMoreData,
-  /// There are enough Frames queue
-  /// May be emitted by `Context::send_frame` when the input queue is constrained
+  /// There are enough frames in the queue.
+  ///
+  /// May be emitted by `rav1e_send_frame` when trying to send a frame after
+  /// the encoder has been flushed.
   EnoughData,
-  /// The encoder already produced the number of frames requested
-  /// May be emitted by `Context::receive_packet` after a flush request had been processed
-  /// or the frame limit had been reached.
+  /// The encoder has already produced the number of frames requested.
+  ///
+  /// May be emitted by `rav1e_receive_packet` after a flush request had been
+  /// processed or the frame limit had been reached.
   LimitReached,
-  /// A Frame had been encoded but not emitted yet
+  /// A Frame had been encoded but not emitted yet.
   Encoded,
-  /// Generic fatal error
+  /// Generic fatal error.
   Failure = -1,
   /// A frame was encoded in the first pass of a 2-pass encode, but its stats
-  /// data was not retrieved with rav1e_twopass_out(), or not enough stats data was
-  /// provided in the second pass of a 2-pass encode to encode the next frame.
+  /// data was not retrieved with `rav1e_twopass_out`, or not enough stats data
+  /// was provided in the second pass of a 2-pass encode to encode the next
+  /// frame.
   NotReady = -2,
 }
 
