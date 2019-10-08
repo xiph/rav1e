254ae21b29945a583aba0d286db56e8bb1ddc0a5
Josh Holmer <jholmer@activecampaign.com>
2019-10-08 10:33:50 -0400

    Remove vestigial x86_64 modules for asm
    Fixes #1741

--- src/asm/x86/dist.rs
+++ src/asm/x86/dist.rs
@@ -13,9 +13,6 @@ use crate::partition::BlockSize;
 use crate::tiling::*;
 use crate::util::*;
 
-#[cfg(all(target_arch = "x86_64", feature = "nasm"))]
-use x86_64::*;
-
 type SadFn = unsafe extern fn(
   src: *const u8,
   src_stride: isize,
@@ -193,142 +190,137 @@ pub fn get_satd<T: Pixel>(
   dist
 }
 
-mod x86_64 {
-  use super::*;
+static SAD_FNS_SSE2: [Option<SadFn>; DIST_FNS_LENGTH] = {
+  let mut out: [Option<SadFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
 
-  static SAD_FNS_SSE2: [Option<SadFn>; DIST_FNS_LENGTH] = {
-    let mut out: [Option<SadFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
+  use BlockSize::*;
 
-    use BlockSize::*;
+  out[BLOCK_4X4 as usize] = Some(rav1e_sad4x4_sse2);
+  out[BLOCK_4X8 as usize] = Some(rav1e_sad4x8_sse2);
+  out[BLOCK_4X16 as usize] = Some(rav1e_sad4x16_sse2);
 
-    out[BLOCK_4X4 as usize] = Some(rav1e_sad4x4_sse2);
-    out[BLOCK_4X8 as usize] = Some(rav1e_sad4x8_sse2);
-    out[BLOCK_4X16 as usize] = Some(rav1e_sad4x16_sse2);
+  out[BLOCK_8X4 as usize] = Some(rav1e_sad8x4_sse2);
+  out[BLOCK_8X8 as usize] = Some(rav1e_sad8x8_sse2);
+  out[BLOCK_8X16 as usize] = Some(rav1e_sad8x16_sse2);
+  out[BLOCK_8X32 as usize] = Some(rav1e_sad8x32_sse2);
 
-    out[BLOCK_8X4 as usize] = Some(rav1e_sad8x4_sse2);
-    out[BLOCK_8X8 as usize] = Some(rav1e_sad8x8_sse2);
-    out[BLOCK_8X16 as usize] = Some(rav1e_sad8x16_sse2);
-    out[BLOCK_8X32 as usize] = Some(rav1e_sad8x32_sse2);
+  out[BLOCK_16X16 as usize] = Some(rav1e_sad16x16_sse2);
+  out[BLOCK_32X32 as usize] = Some(rav1e_sad32x32_sse2);
+  out[BLOCK_64X64 as usize] = Some(rav1e_sad64x64_sse2);
+  out[BLOCK_128X128 as usize] = Some(rav1e_sad128x128_sse2);
 
-    out[BLOCK_16X16 as usize] = Some(rav1e_sad16x16_sse2);
-    out[BLOCK_32X32 as usize] = Some(rav1e_sad32x32_sse2);
-    out[BLOCK_64X64 as usize] = Some(rav1e_sad64x64_sse2);
-    out[BLOCK_128X128 as usize] = Some(rav1e_sad128x128_sse2);
+  out
+};
 
-    out
-  };
+static SAD_FNS_AVX2: [Option<SadFn>; DIST_FNS_LENGTH] = {
+  let mut out: [Option<SadFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
 
-  static SAD_FNS_AVX2: [Option<SadFn>; DIST_FNS_LENGTH] = {
-    let mut out: [Option<SadFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
+  use BlockSize::*;
 
-    use BlockSize::*;
+  out[BLOCK_4X4 as usize] = Some(rav1e_sad4x4_sse2);
+  out[BLOCK_4X8 as usize] = Some(rav1e_sad4x8_sse2);
+  out[BLOCK_4X16 as usize] = Some(rav1e_sad4x16_sse2);
 
-    out[BLOCK_4X4 as usize] = Some(rav1e_sad4x4_sse2);
-    out[BLOCK_4X8 as usize] = Some(rav1e_sad4x8_sse2);
-    out[BLOCK_4X16 as usize] = Some(rav1e_sad4x16_sse2);
+  out[BLOCK_8X4 as usize] = Some(rav1e_sad8x4_sse2);
+  out[BLOCK_8X8 as usize] = Some(rav1e_sad8x8_sse2);
+  out[BLOCK_8X16 as usize] = Some(rav1e_sad8x16_sse2);
+  out[BLOCK_8X32 as usize] = Some(rav1e_sad8x32_sse2);
 
-    out[BLOCK_8X4 as usize] = Some(rav1e_sad8x4_sse2);
-    out[BLOCK_8X8 as usize] = Some(rav1e_sad8x8_sse2);
-    out[BLOCK_8X16 as usize] = Some(rav1e_sad8x16_sse2);
-    out[BLOCK_8X32 as usize] = Some(rav1e_sad8x32_sse2);
+  out[BLOCK_16X4 as usize] = Some(rav1e_sad16x4_avx2);
+  out[BLOCK_16X8 as usize] = Some(rav1e_sad16x8_avx2);
+  out[BLOCK_16X16 as usize] = Some(rav1e_sad16x16_avx2);
+  out[BLOCK_16X32 as usize] = Some(rav1e_sad16x32_avx2);
+  out[BLOCK_16X64 as usize] = Some(rav1e_sad16x64_avx2);
 
-    out[BLOCK_16X4 as usize] = Some(rav1e_sad16x4_avx2);
-    out[BLOCK_16X8 as usize] = Some(rav1e_sad16x8_avx2);
-    out[BLOCK_16X16 as usize] = Some(rav1e_sad16x16_avx2);
-    out[BLOCK_16X32 as usize] = Some(rav1e_sad16x32_avx2);
-    out[BLOCK_16X64 as usize] = Some(rav1e_sad16x64_avx2);
+  out[BLOCK_32X8 as usize] = Some(rav1e_sad32x8_avx2);
+  out[BLOCK_32X16 as usize] = Some(rav1e_sad32x16_avx2);
+  out[BLOCK_32X32 as usize] = Some(rav1e_sad32x32_avx2);
+  out[BLOCK_32X64 as usize] = Some(rav1e_sad32x64_avx2);
 
-    out[BLOCK_32X8 as usize] = Some(rav1e_sad32x8_avx2);
-    out[BLOCK_32X16 as usize] = Some(rav1e_sad32x16_avx2);
-    out[BLOCK_32X32 as usize] = Some(rav1e_sad32x32_avx2);
-    out[BLOCK_32X64 as usize] = Some(rav1e_sad32x64_avx2);
+  out[BLOCK_64X16 as usize] = Some(rav1e_sad64x16_avx2);
+  out[BLOCK_64X32 as usize] = Some(rav1e_sad64x32_avx2);
+  out[BLOCK_64X64 as usize] = Some(rav1e_sad64x64_avx2);
+  out[BLOCK_64X128 as usize] = Some(rav1e_sad64x128_avx2);
 
-    out[BLOCK_64X16 as usize] = Some(rav1e_sad64x16_avx2);
-    out[BLOCK_64X32 as usize] = Some(rav1e_sad64x32_avx2);
-    out[BLOCK_64X64 as usize] = Some(rav1e_sad64x64_avx2);
-    out[BLOCK_64X128 as usize] = Some(rav1e_sad64x128_avx2);
+  out[BLOCK_128X64 as usize] = Some(rav1e_sad128x64_avx2);
+  out[BLOCK_128X128 as usize] = Some(rav1e_sad128x128_avx2);
 
-    out[BLOCK_128X64 as usize] = Some(rav1e_sad128x64_avx2);
-    out[BLOCK_128X128 as usize] = Some(rav1e_sad128x128_avx2);
+  out
+};
 
-    out
-  };
+pub static SAD_FNS: [[Option<SadFn>; DIST_FNS_LENGTH];
+  CpuFeatureLevel::len()] = {
+  let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
 
-  pub static SAD_FNS: [[Option<SadFn>; DIST_FNS_LENGTH];
-    CpuFeatureLevel::len()] = {
-    let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
+  out[CpuFeatureLevel::SSE2 as usize] = SAD_FNS_SSE2;
+  out[CpuFeatureLevel::SSSE3 as usize] = SAD_FNS_SSE2;
+  out[CpuFeatureLevel::AVX2 as usize] = SAD_FNS_AVX2;
 
-    out[CpuFeatureLevel::SSE2 as usize] = SAD_FNS_SSE2;
-    out[CpuFeatureLevel::SSSE3 as usize] = SAD_FNS_SSE2;
-    out[CpuFeatureLevel::AVX2 as usize] = SAD_FNS_AVX2;
+  out
+};
 
-    out
-  };
+static SAD_HBD_FNS_SSSE3: [Option<SadHBDFn>; DIST_FNS_LENGTH] = {
+  let mut out: [Option<SadHBDFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
 
-  static SAD_HBD_FNS_SSSE3: [Option<SadHBDFn>; DIST_FNS_LENGTH] = {
-    let mut out: [Option<SadHBDFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
+  use BlockSize::*;
 
-    use BlockSize::*;
+  out[BLOCK_4X4 as usize] = Some(rav1e_sad_4x4_hbd_ssse3);
+  out[BLOCK_16X16 as usize] = Some(rav1e_sad_16x16_hbd_ssse3);
 
-    out[BLOCK_4X4 as usize] = Some(rav1e_sad_4x4_hbd_ssse3);
-    out[BLOCK_16X16 as usize] = Some(rav1e_sad_16x16_hbd_ssse3);
+  out
+};
 
-    out
-  };
+pub(crate) static SAD_HBD_FNS: [[Option<SadHBDFn>; DIST_FNS_LENGTH];
+  CpuFeatureLevel::len()] = {
+  let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
 
-  pub(crate) static SAD_HBD_FNS: [[Option<SadHBDFn>; DIST_FNS_LENGTH];
-    CpuFeatureLevel::len()] = {
-    let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
+  out[CpuFeatureLevel::SSSE3 as usize] = SAD_HBD_FNS_SSSE3;
+  out[CpuFeatureLevel::AVX2 as usize] = SAD_HBD_FNS_SSSE3;
 
-    out[CpuFeatureLevel::SSSE3 as usize] = SAD_HBD_FNS_SSSE3;
-    out[CpuFeatureLevel::AVX2 as usize] = SAD_HBD_FNS_SSSE3;
+  out
+};
 
-    out
-  };
+static SATD_FNS_AVX2: [Option<SatdFn>; DIST_FNS_LENGTH] = {
+  let mut out: [Option<SatdFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
 
-  static SATD_FNS_AVX2: [Option<SatdFn>; DIST_FNS_LENGTH] = {
-    let mut out: [Option<SatdFn>; DIST_FNS_LENGTH] = [None; DIST_FNS_LENGTH];
+  use BlockSize::*;
 
-    use BlockSize::*;
+  out[BLOCK_4X4 as usize] = Some(rav1e_satd_4x4_avx2);
+  out[BLOCK_8X8 as usize] = Some(rav1e_satd_8x8_avx2);
+  out[BLOCK_16X16 as usize] = Some(rav1e_satd_16x16_avx2);
+  out[BLOCK_32X32 as usize] = Some(rav1e_satd_32x32_avx2);
+  out[BLOCK_64X64 as usize] = Some(rav1e_satd_64x64_avx2);
+  out[BLOCK_128X128 as usize] = Some(rav1e_satd_128x128_avx2);
 
-    out[BLOCK_4X4 as usize] = Some(rav1e_satd_4x4_avx2);
-    out[BLOCK_8X8 as usize] = Some(rav1e_satd_8x8_avx2);
-    out[BLOCK_16X16 as usize] = Some(rav1e_satd_16x16_avx2);
-    out[BLOCK_32X32 as usize] = Some(rav1e_satd_32x32_avx2);
-    out[BLOCK_64X64 as usize] = Some(rav1e_satd_64x64_avx2);
-    out[BLOCK_128X128 as usize] = Some(rav1e_satd_128x128_avx2);
+  out[BLOCK_4X8 as usize] = Some(rav1e_satd_4x8_avx2);
+  out[BLOCK_8X4 as usize] = Some(rav1e_satd_8x4_avx2);
+  out[BLOCK_8X16 as usize] = Some(rav1e_satd_8x16_avx2);
+  out[BLOCK_16X8 as usize] = Some(rav1e_satd_16x8_avx2);
+  out[BLOCK_16X32 as usize] = Some(rav1e_satd_16x32_avx2);
+  out[BLOCK_32X16 as usize] = Some(rav1e_satd_32x16_avx2);
+  out[BLOCK_32X64 as usize] = Some(rav1e_satd_32x64_avx2);
+  out[BLOCK_64X32 as usize] = Some(rav1e_satd_64x32_avx2);
+  out[BLOCK_64X128 as usize] = Some(rav1e_satd_64x128_avx2);
+  out[BLOCK_128X64 as usize] = Some(rav1e_satd_128x64_avx2);
 
-    out[BLOCK_4X8 as usize] = Some(rav1e_satd_4x8_avx2);
-    out[BLOCK_8X4 as usize] = Some(rav1e_satd_8x4_avx2);
-    out[BLOCK_8X16 as usize] = Some(rav1e_satd_8x16_avx2);
-    out[BLOCK_16X8 as usize] = Some(rav1e_satd_16x8_avx2);
-    out[BLOCK_16X32 as usize] = Some(rav1e_satd_16x32_avx2);
-    out[BLOCK_32X16 as usize] = Some(rav1e_satd_32x16_avx2);
-    out[BLOCK_32X64 as usize] = Some(rav1e_satd_32x64_avx2);
-    out[BLOCK_64X32 as usize] = Some(rav1e_satd_64x32_avx2);
-    out[BLOCK_64X128 as usize] = Some(rav1e_satd_64x128_avx2);
-    out[BLOCK_128X64 as usize] = Some(rav1e_satd_128x64_avx2);
+  out[BLOCK_4X16 as usize] = Some(rav1e_satd_4x16_avx2);
+  out[BLOCK_16X4 as usize] = Some(rav1e_satd_16x4_avx2);
+  out[BLOCK_8X32 as usize] = Some(rav1e_satd_8x32_avx2);
+  out[BLOCK_32X8 as usize] = Some(rav1e_satd_32x8_avx2);
+  out[BLOCK_16X64 as usize] = Some(rav1e_satd_16x64_avx2);
+  out[BLOCK_64X16 as usize] = Some(rav1e_satd_64x16_avx2);
 
-    out[BLOCK_4X16 as usize] = Some(rav1e_satd_4x16_avx2);
-    out[BLOCK_16X4 as usize] = Some(rav1e_satd_16x4_avx2);
-    out[BLOCK_8X32 as usize] = Some(rav1e_satd_8x32_avx2);
-    out[BLOCK_32X8 as usize] = Some(rav1e_satd_32x8_avx2);
-    out[BLOCK_16X64 as usize] = Some(rav1e_satd_16x64_avx2);
-    out[BLOCK_64X16 as usize] = Some(rav1e_satd_64x16_avx2);
+  out
+};
 
-    out
-  };
+pub(crate) static SATD_FNS: [[Option<SatdFn>; DIST_FNS_LENGTH];
+  CpuFeatureLevel::len()] = {
+  let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
 
-  pub(crate) static SATD_FNS: [[Option<SatdFn>; DIST_FNS_LENGTH];
-    CpuFeatureLevel::len()] = {
-    let mut out = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
+  out[CpuFeatureLevel::AVX2 as usize] = SATD_FNS_AVX2;
 
-    out[CpuFeatureLevel::AVX2 as usize] = SATD_FNS_AVX2;
+  out
+};
 
-    out
-  };
-
-  pub(crate) static SATD_HBD_FNS: [[Option<SatdHBDFn>; DIST_FNS_LENGTH];
-    CpuFeatureLevel::len()] =
-    [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
-}
+pub(crate) static SATD_HBD_FNS: [[Option<SatdHBDFn>; DIST_FNS_LENGTH];
+  CpuFeatureLevel::len()] = [[None; DIST_FNS_LENGTH]; CpuFeatureLevel::len()];
