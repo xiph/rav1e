ccfeadda18d6edd0a0f5285e20855a56e5eba1b9
David Michael Barr <b@rr-dav.id.au>
2019-10-22 08:35:02 +0900

    Hot fix for Travis cache failure on arm64 (#1789)
    Also, enable sccache on Travis arm64 builds.

--- .travis/install-sccache.sh
+++ .travis/install-sccache.sh
@@ -11,9 +11,7 @@ elif [ "$ARCH" = "x86_64" ]; then
   curl -L "https://github.com/mozilla/sccache/releases/download/$SCCACHE_VERSION/sccache-$SCCACHE_VERSION-x86_64-unknown-linux-musl.tar.gz" | tar xz
   mv -f "sccache-$SCCACHE_VERSION-x86_64-unknown-linux-musl/sccache" "$DEPS_DIR/bin/sccache"
 else
-  # cargo install --version "$SCCACHE_VERSION" --root "$DEPS_DIR" --no-default-features sccache
-  echo "Skipping sccache installation on $ARCH."
-  unset RUSTC_WRAPPER
+  RUSTC_WRAPPER='' cargo install --version "$SCCACHE_VERSION" --root "$DEPS_DIR" --no-default-features sccache
 fi
 
 set +ex
