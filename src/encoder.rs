5746dac3c176469c698be89261e9ac408ca99904
Tristan Matthews <tmatth@videolan.org>
2019-11-14 14:48:04 +0900

    encoder: explicitly define the types of CDEF search

--- src/encoder.rs
+++ src/encoder.rs
@@ -53,6 +53,14 @@ use std::{fmt, io, mem};
 
 use crate::hawktracer::*;
 
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum CDEFSearchMethod {
+  PickFromQ,
+  FastSearch,
+  FullSearch,
+}
+
 pub static TEMPORAL_DELIMITER: [u8; 2] = [0x12, 0x00];
 
 const MAX_NUM_TEMPORAL_LAYERS: usize = 8;
@@ -506,6 +514,7 @@ pub struct FrameInvariants<T: Pixel> {
   pub is_motion_mode_switchable: bool,
   pub disable_frame_end_update_cdf: bool,
   pub allow_warped_motion: bool,
+  pub cdef_search_method: CDEFSearchMethod,
   pub cdef_damping: u8,
   pub cdef_bits: u8,
   pub cdef_y_strengths: [u8; 8],
@@ -670,6 +679,7 @@ impl<T: Pixel> FrameInvariants<T> {
       is_motion_mode_switchable: false, // 0: only the SIMPLE motion mode will be used.
       disable_frame_end_update_cdf: sequence.reduced_still_picture_hdr,
       allow_warped_motion: false,
+      cdef_search_method: CDEFSearchMethod::PickFromQ,
       cdef_damping: 3,
       cdef_bits: 0,
       cdef_y_strengths: [
@@ -901,7 +911,6 @@ impl<T: Pixel> FrameInvariants<T> {
   pub fn set_quantizers(&mut self, qps: &QuantizerParameters) {
     self.base_q_idx = qps.ac_qi[0];
     let base_q_idx = self.base_q_idx as i32;
-    self.cdef_damping = 3 + (self.base_q_idx >> 6);
     for pi in 0..3 {
       debug_assert!(qps.dc_qi[pi] as i32 - base_q_idx >= -128);
       debug_assert!((qps.dc_qi[pi] as i32 - base_q_idx) < 128);
@@ -915,66 +924,73 @@ impl<T: Pixel> FrameInvariants<T> {
     self.me_lambda = self.lambda.sqrt();
     self.dist_scale = qps.dist_scale;
 
-    let q = bexp64(qps.log_target_q as i64 + q57(QSCALE)) as f32;
-    /* These coefficients were trained on libaom. */
-    if !self.intra_only {
-      let predicted_y_f1 = clamp(
-        (-q * q * 0.0000023593946_f32 + q * 0.0068615186_f32 + 0.02709886_f32)
-          .round() as i32,
-        0,
-        15,
-      );
-      let predicted_y_f2 = clamp(
-        (-q * q * 0.00000057629734_f32 + q * 0.0013993345_f32 + 0.03831067_f32)
-          .round() as i32,
-        0,
-        3,
-      );
-      let predicted_uv_f1 = clamp(
-        (-q * q * 0.0000007095069_f32 + q * 0.0034628846_f32 + 0.00887099_f32)
-          .round() as i32,
-        0,
-        15,
-      );
-      let predicted_uv_f2 = clamp(
-        (q * q * 0.00000023874085_f32 + q * 0.00028223585_f32 + 0.05576307_f32)
-          .round() as i32,
-        0,
-        3,
-      );
-      self.cdef_y_strengths[0] =
-        (predicted_y_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_y_f2) as u8;
-      self.cdef_uv_strengths[0] =
-        (predicted_uv_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_uv_f2) as u8;
-    } else {
-      let predicted_y_f1 = clamp(
-        (q * q * 0.0000033731974_f32 + q * 0.008070594_f32 + 0.0187634_f32)
-          .round() as i32,
-        0,
-        15,
-      );
-      let predicted_y_f2 = clamp(
-        (-q * q * -0.0000029167343_f32 + q * 0.0027798624_f32 + 0.0079405_f32)
-          .round() as i32,
-        0,
-        3,
-      );
-      let predicted_uv_f1 = clamp(
-        (-q * q * 0.0000130790995_f32 + q * 0.012892405_f32 - 0.00748388_f32)
-          .round() as i32,
-        0,
-        15,
-      );
-      let predicted_uv_f2 = clamp(
-        (q * q * 0.0000032651783_f32 + q * 0.00035520183_f32 + 0.00228092_f32)
-          .round() as i32,
-        0,
-        3,
-      );
-      self.cdef_y_strengths[0] =
-        (predicted_y_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_y_f2) as u8;
-      self.cdef_uv_strengths[0] =
-        (predicted_uv_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_uv_f2) as u8;
+    match self.cdef_search_method {
+      CDEFSearchMethod::PickFromQ => {
+      self.cdef_damping = 3 + (self.base_q_idx >> 6);
+      let q = bexp64(qps.log_target_q as i64 + q57(QSCALE)) as f32;
+      /* These coefficients were trained on libaom. */
+      if !self.intra_only {
+          let predicted_y_f1 = clamp(
+                  (-q * q * 0.0000023593946_f32 + q * 0.0068615186_f32 + 0.02709886_f32)
+                  .round() as i32,
+                  0,
+                  15,
+                  );
+          let predicted_y_f2 = clamp(
+                  (-q * q * 0.00000057629734_f32 + q * 0.0013993345_f32 + 0.03831067_f32)
+                  .round() as i32,
+                  0,
+                  3,
+                  );
+          let predicted_uv_f1 = clamp(
+                  (-q * q * 0.0000007095069_f32 + q * 0.0034628846_f32 + 0.00887099_f32)
+                  .round() as i32,
+                  0,
+                  15,
+                  );
+          let predicted_uv_f2 = clamp(
+                  (q * q * 0.00000023874085_f32 + q * 0.00028223585_f32 + 0.05576307_f32)
+                  .round() as i32,
+                  0,
+                  3,
+                  );
+          self.cdef_y_strengths[0] =
+              (predicted_y_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_y_f2) as u8;
+          self.cdef_uv_strengths[0] =
+              (predicted_uv_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_uv_f2) as u8;
+      } else {
+          let predicted_y_f1 = clamp(
+                  (q * q * 0.0000033731974_f32 + q * 0.008070594_f32 + 0.0187634_f32)
+                  .round() as i32,
+                  0,
+                  15,
+                  );
+          let predicted_y_f2 = clamp(
+                  (-q * q * -0.0000029167343_f32 + q * 0.0027798624_f32 + 0.0079405_f32)
+                  .round() as i32,
+                  0,
+                  3,
+                  );
+          let predicted_uv_f1 = clamp(
+                  (-q * q * 0.0000130790995_f32 + q * 0.012892405_f32 - 0.00748388_f32)
+                  .round() as i32,
+                  0,
+                  15,
+                  );
+          let predicted_uv_f2 = clamp(
+                  (q * q * 0.0000032651783_f32 + q * 0.00035520183_f32 + 0.00228092_f32)
+                  .round() as i32,
+                  0,
+                  3,
+                  );
+          self.cdef_y_strengths[0] =
+              (predicted_y_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_y_f2) as u8;
+          self.cdef_uv_strengths[0] =
+              (predicted_uv_f1 * CDEF_SEC_STRENGTHS as i32 + predicted_uv_f2) as u8;
+      }
+    }
+      // TODO: implement FastSearch and FullSearch
+    _ => unreachable!(),
     }
   }
 
