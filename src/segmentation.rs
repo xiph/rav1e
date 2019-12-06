51a1b7e032ad9b43bedf962bc73436ef364bbb6f
Yushin Cho <ycho@mozilla.com>
2019-12-06 13:39:21 -0800

    Add an enable/disable switch for using segmentation
    By default, segmentation is turned off.
    Note that there is an existing similar flag 'segmentation.enabled' under
    SegmentationState struct under TileState, and the new one from this commit is
    the master switch and later will be controllable by CLI.
    
    By not using any segment, this temporarilly fixes the ongoing desync issues caused by quantizer rdo and
    heuristic quantizer choice at speed 0,1,2.

--- src/segmentation.rs
+++ src/segmentation.rs
@@ -16,49 +16,53 @@ use crate::FrameState;
 pub fn segmentation_optimize<T: Pixel>(
   fi: &FrameInvariants<T>, fs: &mut FrameState<T>,
 ) {
+  assert!(fi.enable_segmentation);
   fs.segmentation.enabled = true;
-  fs.segmentation.update_map = true;
 
-  // We don't change the values between frames.
-  fs.segmentation.update_data = fi.primary_ref_frame == PRIMARY_REF_NONE;
-
-  if !fs.segmentation.update_data {
-    return;
-  }
-
-  // A series of AWCY runs with deltas 13, 15, 17, 18, 19, 20, 21, 22, 23
-  // showed this to be the optimal one.
-  const TEMPORAL_RDO_QI_DELTA: i16 = 21;
-
-  // Avoid going into lossless mode by never bringing qidx below 1.
-  // Because base_q_idx changes more frequently than the segmentation
-  // data, it is still possible for a segment to enter lossless, so
-  // enforcement elsewhere is needed.
-  let offset_lower_limit = 1 - fi.base_q_idx as i16;
-
-  // Fill in 3 slots with 0, delta, -delta. The slot IDs are also used in
-  // luma_chroma_mode_rdo() so if you change things here make sure to check
-  // that place too.
-  for i in 0..3 {
-    fs.segmentation.features[i][SegLvl::SEG_LVL_ALT_Q as usize] = true;
-    fs.segmentation.data[i][SegLvl::SEG_LVL_ALT_Q as usize] = match i {
-      0 => 0,
-      1 => TEMPORAL_RDO_QI_DELTA,
-      2 => (-TEMPORAL_RDO_QI_DELTA).max(offset_lower_limit),
-      _ => unreachable!(),
-    };
-  }
-
-  /* Figure out parameters */
-  fs.segmentation.preskip = false;
-  fs.segmentation.last_active_segid = 0;
   if fs.segmentation.enabled {
-    for i in 0..8 {
-      for j in 0..SegLvl::SEG_LVL_MAX as usize {
-        if fs.segmentation.features[i][j] {
-          fs.segmentation.last_active_segid = i as u8;
-          if j >= SegLvl::SEG_LVL_REF_FRAME as usize {
-            fs.segmentation.preskip = true;
+    fs.segmentation.update_map = true;
+
+    // We don't change the values between frames.
+    fs.segmentation.update_data = fi.primary_ref_frame == PRIMARY_REF_NONE;
+
+    if !fs.segmentation.update_data {
+      return;
+    }
+
+    // A series of AWCY runs with deltas 13, 15, 17, 18, 19, 20, 21, 22, 23
+    // showed this to be the optimal one.
+    const TEMPORAL_RDO_QI_DELTA: i16 = 21;
+
+    // Avoid going into lossless mode by never bringing qidx below 1.
+    // Because base_q_idx changes more frequently than the segmentation
+    // data, it is still possible for a segment to enter lossless, so
+    // enforcement elsewhere is needed.
+    let offset_lower_limit = 1 - fi.base_q_idx as i16;
+
+    // Fill in 3 slots with 0, delta, -delta. The slot IDs are also used in
+    // luma_chroma_mode_rdo() so if you change things here make sure to check
+    // that place too.
+    for i in 0..3 {
+      fs.segmentation.features[i][SegLvl::SEG_LVL_ALT_Q as usize] = true;
+      fs.segmentation.data[i][SegLvl::SEG_LVL_ALT_Q as usize] = match i {
+        0 => 0,
+        1 => TEMPORAL_RDO_QI_DELTA,
+        2 => (-TEMPORAL_RDO_QI_DELTA).max(offset_lower_limit),
+        _ => unreachable!(),
+      };
+    }
+
+    /* Figure out parameters */
+    fs.segmentation.preskip = false;
+    fs.segmentation.last_active_segid = 0;
+    if fs.segmentation.enabled {
+      for i in 0..8 {
+        for j in 0..SegLvl::SEG_LVL_MAX as usize {
+          if fs.segmentation.features[i][j] {
+            fs.segmentation.last_active_segid = i as u8;
+            if j >= SegLvl::SEG_LVL_REF_FRAME as usize {
+              fs.segmentation.preskip = true;
+            }
           }
         }
       }
