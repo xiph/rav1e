1c8de59a4ddedd8a725aeb0f63ec45856d6935ec
Josh Holmer <jholmer.in@gmail.com>
2019-12-27 23:06:41 -0500

    Add benchmarks for sse_wxh

--- benches/rdo.rs
+++ benches/rdo.rs
@@ -30,4 +30,65 @@ pub fn cdef_dist_wxh_8x8(c: &mut Criterion) {
   });
 }
 
-criterion_group!(rdo, cdef_dist_wxh_8x8);
+pub fn sse_wxh_8x8(c: &mut Criterion) {
+  let src1 = init_plane_u8(8, 8, 1);
+  let src2 = init_plane_u8(8, 8, 2);
+
+  c.bench_function("sse_wxh_8x8", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 8, height: 8 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 8, height: 8 }),
+        8,
+        8,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+pub fn sse_wxh_4x4(c: &mut Criterion) {
+  let src1 = init_plane_u8(8, 8, 1);
+  let src2 = init_plane_u8(8, 8, 2);
+
+  c.bench_function("sse_wxh_4z4", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        4,
+        4,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+pub fn sse_wxh_2x2(c: &mut Criterion) {
+  let mut src1 = init_plane_u8(8, 8, 1);
+  let mut src2 = init_plane_u8(8, 8, 2);
+  src1.cfg.xdec = 1;
+  src1.cfg.ydec = 1;
+  src2.cfg.xdec = 1;
+  src2.cfg.ydec = 1;
+
+  c.bench_function("sse_wxh_2x2", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        4,
+        4,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+criterion_group!(
+  rdo,
+  cdef_dist_wxh_8x8,
+  sse_wxh_8x8,
+  sse_wxh_4x4,
+  sse_wxh_2x2
+);
