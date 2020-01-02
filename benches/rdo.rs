f4ec653d0996704e9f0e86afc5f7799ca1f10a04
Josh Holmer <jholmer@activecampaign.com>
2020-01-02 14:35:30 -0500

    Add HBD benchmarks for sse_wxh

--- benches/rdo.rs
+++ benches/rdo.rs
@@ -17,6 +17,12 @@ fn init_plane_u8(width: usize, height: usize, seed: u8) -> Plane<u8> {
   Plane::wrap(data, width)
 }
 
+fn init_plane_u16(width: usize, height: usize, seed: u8) -> Plane<u16> {
+  let mut ra = ChaChaRng::from_seed([seed; 32]);
+  let data: Vec<u16> = (0..(width * height)).map(|_| ra.gen()).collect();
+  Plane::wrap(data, width)
+}
+
 pub fn cdef_dist_wxh_8x8(c: &mut Criterion) {
   let src1 = init_plane_u8(8, 8, 1);
   let src2 = init_plane_u8(8, 8, 2);
@@ -78,6 +84,61 @@ pub fn sse_wxh_2x2(c: &mut Criterion) {
   src2.cfg.ydec = 1;
 
   c.bench_function("sse_wxh_2x2", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        4,
+        4,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+pub fn sse_wxh_hbd_8x8(c: &mut Criterion) {
+  let src1 = init_plane_u16(8, 8, 1);
+  let src2 = init_plane_u16(8, 8, 2);
+
+  c.bench_function("sse_wxh_hbd_8x8", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 8, height: 8 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 8, height: 8 }),
+        8,
+        8,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+pub fn sse_wxh_hbd_4x4(c: &mut Criterion) {
+  let src1 = init_plane_u16(8, 8, 1);
+  let src2 = init_plane_u16(8, 8, 2);
+
+  c.bench_function("sse_wxh_hbd_4z4", move |b| {
+    b.iter(|| {
+      rdo::sse_wxh(
+        &src1.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        &src2.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
+        4,
+        4,
+        |_, _| 1.0,
+      )
+    })
+  });
+}
+
+pub fn sse_wxh_hbd_2x2(c: &mut Criterion) {
+  let mut src1 = init_plane_u16(8, 8, 1);
+  let mut src2 = init_plane_u16(8, 8, 2);
+  src1.cfg.xdec = 1;
+  src1.cfg.ydec = 1;
+  src2.cfg.xdec = 1;
+  src2.cfg.ydec = 1;
+
+  c.bench_function("sse_wxh_hbd_2x2", move |b| {
     b.iter(|| {
       rdo::sse_wxh(
         &src1.region(Area::Rect { x: 0, y: 0, width: 4, height: 4 }),
@@ -154,6 +215,9 @@ criterion_group!(
   sse_wxh_8x8,
   sse_wxh_4x4,
   sse_wxh_2x2,
+  sse_wxh_hbd_8x8,
+  sse_wxh_hbd_4x4,
+  sse_wxh_hbd_2x2,
   compute_distortion_scale_8x8,
   compute_distortion_scale_64x64
 );
