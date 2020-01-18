91b25c56e9f21e2329f80789faf8236253d2daa3
Yushin Cho <ycho@mozilla.com>
2020-01-17 17:01:25 -0800

    Remove obsolete tx_set types
    Remove three obsolete tx_set types, i.e.
    TX_SET_DTT4_IDTX_1DDCT_16X16,
    TX_SET_DTT9_IDTX and
    TX_SET_ALL16_16X16.

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -242,17 +242,9 @@ pub enum TxSet {
   // Discrete Trig transforms w/o flip (4) + Identity (1)
   TX_SET_DTT4_IDTX, // TX_SET_INTRA_2
   // Discrete Trig transforms w/o flip (4) + Identity (1) + 1D Hor/vert DCT (2)
-  // for 16x16 only
-  TX_SET_DTT4_IDTX_1DDCT_16X16,
-  // Discrete Trig transforms w/o flip (4) + Identity (1) + 1D Hor/vert DCT (2)
   TX_SET_DTT4_IDTX_1DDCT, // TX_SET_INTRA_1
-  // Discrete Trig transforms w/ flip (9) + Identity (1)
-  TX_SET_DTT9_IDTX,
   // Discrete Trig transforms w/ flip (9) + Identity (1) + 1D Hor/Ver DCT (2)
   TX_SET_DTT9_IDTX_1DDCT, // TX_SET_INTER_2
-  // Discrete Trig transforms w/ flip (9) + Identity (1) + 1D Hor/Ver (6)
-  // for 16x16 only
-  TX_SET_ALL16_16X16,
   // Discrete Trig transforms w/ flip (9) + Identity (1) + 1D Hor/Ver (6)
   TX_SET_ALL16, // TX_SET_INTER_1
 }
