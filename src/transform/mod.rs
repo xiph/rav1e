a8cd519d4dc997e08cd5be7cef18cb21a08ad1b5
Yushin Cho <ycho@mozilla.com>
2020-01-18 22:33:16 -0800

    Use tx_set names defined in av1 spec
    https://aomediacodec.github.io/av1-spec/#transform-type-semantics

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -238,15 +238,15 @@ pub enum TxSet {
   // DCT only
   TX_SET_DCTONLY,
   // DCT + Identity only
-  TX_SET_DCT_IDTX, // TX_SET_INTER_3
+  TX_SET_INTER_3, // TX_SET_DCT_IDTX
   // Discrete Trig transforms w/o flip (4) + Identity (1)
-  TX_SET_DTT4_IDTX, // TX_SET_INTRA_2
+  TX_SET_INTRA_2, // TX_SET_DTT4_IDTX
   // Discrete Trig transforms w/o flip (4) + Identity (1) + 1D Hor/vert DCT (2)
-  TX_SET_DTT4_IDTX_1DDCT, // TX_SET_INTRA_1
+  TX_SET_INTRA_1, // TX_SET_DTT4_IDTX_1DDCT
   // Discrete Trig transforms w/ flip (9) + Identity (1) + 1D Hor/Ver DCT (2)
-  TX_SET_DTT9_IDTX_1DDCT, // TX_SET_INTER_2
+  TX_SET_INTER_2, // TX_SET_DTT9_IDTX_1DDCT
   // Discrete Trig transforms w/ flip (9) + Identity (1) + 1D Hor/Ver (6)
-  TX_SET_ALL16, // TX_SET_INTER_1
+  TX_SET_INTER_1, // TX_SET_ALL16
 }
 
 /// Utility function that returns the log of the ratio of the col and row sizes.
