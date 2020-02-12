4ff96fa1b7b604a8ccf63a5e682b2a512a03ffe9
Lynne <dev@lynne.ee>
2020-02-12 14:49:55 +0000

    After waiting for an hour new tuning has arrived, rejoice!

--- src/rdo.rs
+++ src/rdo.rs
@@ -184,7 +184,7 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
     * (svar + svar + (16_384 << (2 * coeff_shift))) as f64
     / f64::sqrt(((16_265_089i64 << (4 * coeff_shift)) + svar * svar) as f64);
 
-  let ssim_boost = ssim_boost * 0.828345441251f64 + 0.232321716055f64;
+  let ssim_boost = ssim_boost * 0.917972192141f64 + 0.144207336938f64;
 
   RawDistortion::new((sse * ssim_boost + 0.5_f64) as u64)
 }
