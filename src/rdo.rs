a5d8284b56b17f8c80b5e0edf91f51eb1c7cb938
Lynne <dev@lynne.ee>
2020-02-13 12:58:50 +0000

    handtune attempt 2

--- src/rdo.rs
+++ src/rdo.rs
@@ -185,7 +185,7 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
     / f64::sqrt(((16_265_089i64 << (4 * coeff_shift)) + svar * svar) as f64);
 
   /* Tuned on 2600468480 samples of various images and videos */
-  let ssim_boost = ssim_boost * 1.0f64;
+  let ssim_boost = ssim_boost * 1.0f64 + 0f64;
 
   RawDistortion::new((sse * ssim_boost + 0.5_f64) as u64)
 }
