fc0ab4d561dbce8ba190e812f714e485d44195b9
David Michael Barr <b@rr-dav.id.au>
2020-03-05 18:59:25 +0900

    CI: Update libdav1d to 0.5.2-dmo2

--- .travis/install-dav1d.sh
+++ .travis/install-dav1d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-DAV1D_VERSION="0.5.2-dmo1"
+DAV1D_VERSION="0.5.2-dmo2"
 PKG_URL="http://www.deb-multimedia.org/pool/main/d/dav1d-dmo"
 
 case "$ARCH" in
@@ -17,10 +17,10 @@ curl -O "$PKG_URL/libdav1d-dev_${DAV1D_VERSION}_$ARCH.deb" \
      -O "$PKG_URL/libdav1d3_${DAV1D_VERSION}_$ARCH.deb"
 
 sha256sum --check --ignore-missing <<EOF
-9d9618445d5f79867d7aa2e338e11b48386c961a3c58002523efe2bad3b3f815  libdav1d3_0.5.2-dmo1_amd64.deb
-fb0e7d592fa2324c52ac3fb6f79d4ff1ec01923280afcac1bdd74beb174532ca  libdav1d3_0.5.2-dmo1_arm64.deb
-9f61ffe439c95a934996a6c35ab986983bb36d93057f8d1c958fd839982b6cb1  libdav1d-dev_0.5.2-dmo1_amd64.deb
-d2af1ebe3d953a07dc9512f45aac62be2622d361b3262faf71ac2ed9cb4bbfdd  libdav1d-dev_0.5.2-dmo1_arm64.deb
+918e83902927c9fbb17023a8973ecfea8876ac0deb2f5ffadf1d8cbbcbd4472f  libdav1d3_0.5.2-dmo2_amd64.deb
+fd3e85c300b1b0f75b2092a0be694256e452e720b9d37e457a8cfb66cdbdbfb9  libdav1d3_0.5.2-dmo2_arm64.deb
+c303be29c114d79b25f61b86b4e82fccbc748fbfb2484c5ba8f3936e5d8e90b7  libdav1d-dev_0.5.2-dmo2_amd64.deb
+bb164a093e43172e48e47514c741974301e4120f98cf9a8411e21a47d4fecf9f  libdav1d-dev_0.5.2-dmo2_arm64.deb
 EOF
 
 sudo dpkg -i "libdav1d3_${DAV1D_VERSION}_$ARCH.deb" \
