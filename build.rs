85a440497b2a1b53b5de0c466ba31c2c7c1658c8
Luca Barbato <lu_zero@gentoo.org>
2020-04-11 07:59:31 +0200

    Do not try to rebuild the assembly files if not needed
    The build is triggered only if the assembly files or the configuration
    changes.

--- build.rs
+++ build.rs
@@ -14,7 +14,7 @@ use rustc_version::{version, Version};
 #[allow(unused_imports)]
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
 #[allow(dead_code)]
@@ -30,44 +30,87 @@ fn rerun_dir<P: AsRef<Path>>(dir: P) {
   }
 }
 
+#[allow(dead_code)]
+fn hash_changed(
+  files: &[&str], out_dir: &str, config: &Path,
+) -> Option<([u8; 8], PathBuf)> {
+  use std::collections::hash_map::DefaultHasher;
+  use std::hash::Hasher;
+  use std::io::Read;
+
+  let mut hasher = DefaultHasher::new();
+
+  let paths = files.iter().map(Path::new).chain(std::iter::once(config));
+
+  for path in paths {
+    if let Ok(mut f) = std::fs::File::open(path) {
+      let mut buf = Vec::new();
+      f.read_to_end(&mut buf).unwrap();
+
+      hasher.write(&buf);
+    } else {
+      panic!("Cannot open {}", path.display());
+    }
+  }
+
+  let hash = hasher.finish().to_be_bytes();
+
+  let hash_path = Path::new(&out_dir).join("asm.hash");
+
+  if let Ok(old_hash) = std::fs::read(&hash_path) {
+    if old_hash == hash {
+      return None;
+    }
+  }
+
+  Some((hash, hash_path))
+}
+
 #[cfg(feature = "asm")]
 fn build_nasm_files() {
   use std::fs::File;
   use std::io::Write;
   let out_dir = env::var("OUT_DIR").unwrap();
-  {
-    let dest_path = Path::new(&out_dir).join("config.asm");
-    let mut config_file = File::create(dest_path).unwrap();
-    config_file.write(b"	%define private_prefix rav1e\n").unwrap();
-    config_file.write(b"	%define ARCH_X86_32 0\n").unwrap();
-    config_file.write(b" %define ARCH_X86_64 1\n").unwrap();
-    config_file.write(b"	%define PIC 1\n").unwrap();
-    config_file.write(b" %define STACK_ALIGNMENT 16\n").unwrap();
-    if cfg!(target_os = "macos") {
-      config_file.write(b" %define PREFIX 1\n").unwrap();
-    }
+
+  let dest_path = Path::new(&out_dir).join("config.asm");
+  let mut config_file = File::create(&dest_path).unwrap();
+  config_file.write(b"	%define private_prefix rav1e\n").unwrap();
+  config_file.write(b"	%define ARCH_X86_32 0\n").unwrap();
+  config_file.write(b" %define ARCH_X86_64 1\n").unwrap();
+  config_file.write(b"	%define PIC 1\n").unwrap();
+  config_file.write(b" %define STACK_ALIGNMENT 16\n").unwrap();
+  if cfg!(target_os = "macos") {
+    config_file.write(b" %define PREFIX 1\n").unwrap();
   }
-  let mut config_include_arg = String::from("-I");
-  config_include_arg.push_str(&out_dir);
-  config_include_arg.push('/');
-  nasm_rs::compile_library_args(
-    "rav1easm",
-    &[
-      "src/x86/ipred.asm",
-      "src/x86/ipred_ssse3.asm",
-      "src/x86/itx.asm",
-      "src/x86/itx_ssse3.asm",
-      "src/x86/mc.asm",
-      "src/x86/mc_ssse3.asm",
-      "src/x86/me.asm",
-      "src/x86/sad_sse2.asm",
-      "src/x86/sad_avx.asm",
-      "src/x86/satd.asm",
-      "src/x86/cdef.asm",
-      "src/x86/tables.asm",
-    ],
-    &[&config_include_arg, "-Isrc/"],
-  );
+
+  let asm_files = &[
+    "src/x86/ipred.asm",
+    "src/x86/ipred_ssse3.asm",
+    "src/x86/itx.asm",
+    "src/x86/itx_ssse3.asm",
+    "src/x86/mc.asm",
+    "src/x86/mc_ssse3.asm",
+    "src/x86/me.asm",
+    "src/x86/sad_sse2.asm",
+    "src/x86/sad_avx.asm",
+    "src/x86/satd.asm",
+    "src/x86/cdef.asm",
+    "src/x86/tables.asm",
+  ];
+
+  if let Some((hash, hash_path)) =
+    hash_changed(asm_files, &out_dir, &dest_path)
+  {
+    let mut config_include_arg = String::from("-I");
+    config_include_arg.push_str(&out_dir);
+    config_include_arg.push('/');
+    nasm_rs::compile_library_args(
+      "rav1easm",
+      asm_files,
+      &[&config_include_arg, "-Isrc/"],
+    );
+    std::fs::write(hash_path, &hash[..]).unwrap();
+  }
   println!("cargo:rustc-link-lib=static=rav1easm");
   rerun_dir("src/x86");
   rerun_dir("src/ext/x86");
@@ -78,25 +121,34 @@ fn build_asm_files() {
   use std::fs::File;
   use std::io::Write;
   let out_dir = env::var("OUT_DIR").unwrap();
+
+  let dest_path = Path::new(&out_dir).join("config.h");
+  let mut config_file = File::create(&dest_path).unwrap();
+  config_file.write(b" #define PRIVATE_PREFIX rav1e_\n").unwrap();
+  config_file.write(b" #define ARCH_AARCH64 1\n").unwrap();
+  config_file.write(b" #define ARCH_ARM 0\n").unwrap();
+  config_file.write(b" #define CONFIG_LOG 1 \n").unwrap();
+  config_file.write(b" #define HAVE_ASM 1\n").unwrap();
+  config_file.sync_all().unwrap();
+
+  let asm_files = &[
+    "src/arm/64/mc.S",
+    "src/arm/64/itx.S",
+    "src/arm/64/ipred.S",
+    "src/arm/tables.S",
+  ];
+
+  if let Some((hash, hash_path)) =
+    hash_changed(asm_files, &out_dir, &dest_path)
   {
-    let dest_path = Path::new(&out_dir).join("config.h");
-    let mut config_file = File::create(dest_path).unwrap();
-    config_file.write(b" #define PRIVATE_PREFIX rav1e_\n").unwrap();
-    config_file.write(b" #define ARCH_AARCH64 1\n").unwrap();
-    config_file.write(b" #define ARCH_ARM 0\n").unwrap();
-    config_file.write(b" #define CONFIG_LOG 1 \n").unwrap();
-    config_file.write(b" #define HAVE_ASM 1\n").unwrap();
+    cc::Build::new()
+      .files(asm_files)
+      .include(".")
+      .include(&out_dir)
+      .compile("rav1e-aarch64");
+
+    std::fs::write(hash_path, &hash[..]).unwrap();
   }
-  cc::Build::new()
-    .files(&[
-      "src/arm/64/mc.S",
-      "src/arm/64/itx.S",
-      "src/arm/64/ipred.S",
-      "src/arm/tables.S",
-    ])
-    .include(".")
-    .include(&out_dir)
-    .compile("rav1e-aarch64");
   rerun_dir("src/arm");
 }
 
