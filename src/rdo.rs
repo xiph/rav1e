8f46e04d92feffc929fa2cb7a0811f6e4f55b913
David Michael Barr <b@rr-dav.id.au>
2020-05-14 10:50:22 +0900

    Split fine directional prediction search into 2 passes
    1. Search matching deltas for luma and chroma
    2. Refine chroma delta for best luma delta
    
    Iterations are the sum of luma/chroma deltas rather than the product.

--- src/rdo.rs
+++ src/rdo.rs
@@ -1369,46 +1369,72 @@ fn intra_frame_rdo_mode_decision<T: Pixel>(
     let luma_angle_delta_count = best.pred_mode_luma.angle_delta_count();
     let chroma_angle_delta_count = best.pred_mode_chroma.angle_delta_count();
 
-    'luma_loop: for i in 0..luma_angle_delta_count {
-      for j in 0..chroma_angle_delta_count {
-        let mvs = [MotionVector::default(); 2];
-        let ref_frames = [INTRA_FRAME, NONE_FRAME];
-        let mut mode_set_chroma = ArrayVec::<[_; 2]>::new();
-        mode_set_chroma.push(best.pred_mode_chroma);
+    let mvs = [MotionVector::default(); 2];
+    let ref_frames = [INTRA_FRAME, NONE_FRAME];
+    let mode_set_chroma = [best.pred_mode_chroma];
 
-        let angle_delta_y: i8 = if luma_angle_delta_count == 1 {
-          0
-        } else {
-          i - MAX_ANGLE_DELTA as i8
-        };
-        let angle_delta_uv: i8 = if chroma_angle_delta_count == 1 {
-          0
-        } else {
-          j - MAX_ANGLE_DELTA as i8
-        };
-        if luma_angle_delta_count == 1 && chroma_angle_delta_count == 1 {
-          break 'luma_loop;
-        }
-
-        luma_chroma_mode_rdo(
-          best.pred_mode_luma,
-          fi,
-          bsize,
-          tile_bo,
-          ts,
-          cw,
-          rdo_type,
-          cw_checkpoint,
-          &mut best,
-          mvs,
-          ref_frames,
-          &mode_set_chroma,
-          true,
-          0,
-          &ArrayVec::<[CandidateMV; 9]>::new(),
-          AngleDelta { y: angle_delta_y, uv: angle_delta_uv },
-        );
+    for i in 0..luma_angle_delta_count {
+      let angle_delta_y: i8 = if luma_angle_delta_count == 1 {
+        0
+      } else {
+        i - MAX_ANGLE_DELTA as i8
+      };
+      let angle_delta_uv: i8 =
+        if chroma_angle_delta_count == 1 { 0 } else { angle_delta_y };
+      if angle_delta_y == best.angle_delta.y
+        && angle_delta_uv == best.angle_delta.uv
+      {
+        continue;
       }
+
+      luma_chroma_mode_rdo(
+        best.pred_mode_luma,
+        fi,
+        bsize,
+        tile_bo,
+        ts,
+        cw,
+        rdo_type,
+        cw_checkpoint,
+        &mut best,
+        mvs,
+        ref_frames,
+        &mode_set_chroma,
+        true,
+        0,
+        &ArrayVec::<[CandidateMV; 9]>::new(),
+        AngleDelta { y: angle_delta_y, uv: angle_delta_uv },
+      );
+    }
+    for j in 0..chroma_angle_delta_count {
+      let angle_delta_y: i8 = best.angle_delta.y;
+      let angle_delta_uv: i8 = if chroma_angle_delta_count == 1 {
+        0
+      } else {
+        j - MAX_ANGLE_DELTA as i8
+      };
+      if angle_delta_uv == best.angle_delta.uv {
+        continue;
+      }
+
+      luma_chroma_mode_rdo(
+        best.pred_mode_luma,
+        fi,
+        bsize,
+        tile_bo,
+        ts,
+        cw,
+        rdo_type,
+        cw_checkpoint,
+        &mut best,
+        mvs,
+        ref_frames,
+        &mode_set_chroma,
+        true,
+        0,
+        &ArrayVec::<[CandidateMV; 9]>::new(),
+        AngleDelta { y: angle_delta_y, uv: angle_delta_uv },
+      );
     }
   }
 
