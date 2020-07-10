a2e43e0a3da506848a4d0c3b9202ca3346c8432d
Takehiro Kajihara <takehiro6021@gmail.com>
2020-07-11 08:47:45 +0900

    Fix assert failure in bench of downsample_odd
    * Fix assert failure in bench of downsample_odd
    
    * Fix downsampled() args to the original frame size in bench

--- benches/plane.rs
+++ benches/plane.rs
@@ -6,7 +6,21 @@ use rav1e::bench::frame::*;
 fn init_plane_u8(width: usize, height: usize) -> Plane<u8> {
   let mut ra = ChaChaRng::from_seed([0; 32]);
   let data: Vec<u8> = (0..(width * height)).map(|_| ra.gen()).collect();
-  Plane::from_slice(&data, width)
+  let out = Plane::from_slice(&data, width);
+  if out.cfg.width % 2 == 0 && out.cfg.height % 2 == 0 {
+    out
+  } else {
+    let xpad = out.cfg.width % 2;
+    let ypad = out.cfg.height % 2;
+    let mut padded =
+      Plane::new(out.cfg.width, out.cfg.height, 0, 0, xpad, ypad);
+    let mut padded_slice = padded.mut_slice(PlaneOffset { x: 0, y: 0 });
+    for (dst_row, src_row) in padded_slice.rows_iter_mut().zip(out.rows_iter())
+    {
+      dst_row[..out.cfg.width].copy_from_slice(&src_row[..out.cfg.width]);
+    }
+    padded
+  }
 }
 
 fn init_plane_u16(width: usize, height: usize) -> Plane<u16> {
@@ -19,7 +33,7 @@ pub fn downsample_8bit(c: &mut Criterion) {
   let input = init_plane_u8(1920, 1080);
   c.bench_function("downsample_8bit", move |b| {
     b.iter(|| {
-      let _ = input.downsampled(960, 540);
+      let _ = input.downsampled(input.cfg.width, input.cfg.height);
     })
   });
 }
@@ -28,7 +42,7 @@ pub fn downsample_odd(c: &mut Criterion) {
   let input = init_plane_u8(1919, 1079);
   c.bench_function("downsample_odd", move |b| {
     b.iter(|| {
-      let _ = input.downsampled(960, 540);
+      let _ = input.downsampled(input.cfg.width, input.cfg.height);
     })
   });
 }
@@ -37,7 +51,7 @@ pub fn downsample_10bit(c: &mut Criterion) {
   let input = init_plane_u16(1920, 1080);
   c.bench_function("downsample_10bit", move |b| {
     b.iter(|| {
-      let _ = input.downsampled(960, 540);
+      let _ = input.downsampled(input.cfg.width, input.cfg.height);
     })
   });
 }
