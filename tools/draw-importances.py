0af1b6d3c4d7822cddd23d496fc3949f923b57bf
Luni-4 <luni-4@hotmail.it>
2020-07-23 18:11:25 +0200

    Format tools

--- tools/draw-importances.py
+++ tools/draw-importances.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
+
+import datetime
+import glob
+import os
+import re
 import struct
 import sys
-import os
 from os.path import splitext
-import datetime
-import numpy as np
-from PIL import Image, ImageDraw
-from matplotlib import pyplot as plt
+
 import click
-import glob
-import re
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw
+
 
 @click.command()
-@click.option('--path',  type = str,
-        help = "Absolute File locations of extracted hres.png and imps.bin ")
-@click.option('--input', help="Pass both hres.png and imps.bin",
-                default=[None] * 2, type=click.Tuple([str, str]))
-
-@click.option('--verbose', is_flag=True, help="Will print verbose messages.")
-@click.option('--raw', is_flag=True, help="Print RAW Data of the bin.")
-@click.option('--figure', is_flag=True, help="Draw Importance to frame and save as PNG.")
-@click.option('--csv', type = str, help="Export as frame_type, importance as CSV.")
-
-
+@click.option(
+    "--path",
+    type=str,
+    help="Absolute File locations of extracted hres.png and imps.bin ",
+)
+@click.option(
+    "--input",
+    help="Pass both hres.png and imps.bin",
+    default=[None] * 2,
+    type=click.Tuple([str, str]),
+)
+@click.option("--verbose", is_flag=True, help="Will print verbose messages.")
+@click.option("--raw", is_flag=True, help="Print RAW Data of the bin.")
+@click.option(
+    "--figure", is_flag=True, help="Draw Importance to frame and save as PNG."
+)
+@click.option(
+    "--csv", type=str, help="Export as frame_type, importance as CSV."
+)
 def blockImportance(input, verbose, path, figure, raw, csv):
     """
     CLI tool for extracting rav1e's Block Importance
@@ -42,37 +53,44 @@ def blockImportance(input, verbose, path, figure, raw, csv):
     Note: CSV Mode only available for Batch mode.
     """
 
-    folder_path = "out/"+ datetime.datetime.now().strftime('%Y%m%d_%H')
+    folder_path = "out/" + datetime.datetime.now().strftime("%Y%m%d_%H")
     if figure or csv:
-        if not os.path.exists('out'):
-            os.mkdir('out')
+        if not os.path.exists("out"):
+            os.mkdir("out")
 
         if not os.path.exists(folder_path):
             os.mkdir(folder_path)
 
-    if input == (None, None) and path == None :
-        raise click.BadParameter("Please use either --input or --path method, for more information use --help.")
+    if input == (None, None) and path == None:
+        raise click.BadParameter(
+            "Please use either --input or --path method, "
+            "for more information use --help."
+        )
 
     if verbose:
-        click.echo("Input given: "+ str(input))
+        click.echo("Input given: " + str(input))
     if path:
         click.echo("Path given: " + str(path))
 
     if input != (None, None):
         if csv:
-           raise click.BadParameter("Not Enough data to export as CSV, use Batch mode or remove --csv.")
+            raise click.BadParameter(
+                "Not Enough data to export as CSV, "
+                "use Batch mode or remove --csv."
+            )
         if verbose:
             click.secho("MODE 1", fg="yellow")
-        with open(input[1], 'rb') as f:
+        with open(input[1], "rb") as f:
             contents = f.read()
 
-        rows, cols, frame_type = struct.unpack('qqq', contents[:24])
-        imps = np.frombuffer(contents[24:],
-                            dtype=np.float32).reshape((rows, cols))
+        rows, cols, frame_type = struct.unpack("qqq", contents[:24])
+        imps = np.frombuffer(contents[24:], dtype=np.float32).reshape(
+            (rows, cols)
+        )
 
         if verbose:
-            click.echo("Frame Type: "+ str(frame_type))
-            click.echo("Mean Importance: "+ str(np.mean(imps)))
+            click.echo("Frame Type: " + str(frame_type))
+            click.echo("Mean Importance: " + str(np.mean(imps)))
         if raw:
             click.secho("imps data after processing:", fg="red")
             click.echo(imps)
@@ -85,18 +103,21 @@ def blockImportance(input, verbose, path, figure, raw, csv):
         rows_list = []
         col_list = []
         frame_type_list = []
-        mean_list= []
-        png_list = sorted(glob.glob(str(path)+"/*hres.png"))
+        mean_list = []
+        png_list = sorted(glob.glob(str(path) + "/*hres.png"))
         for png_iter in png_list:
-             bin_list.append(png_iter.replace("hres.png","imps.bin"))
+            bin_list.append(png_iter.replace("hres.png", "imps.bin"))
         total_files = len(bin_list)
 
         for bin in bin_list:
-            with open(bin, 'rb') as f:
-                contents_batch = (f.read())
-                rows, cols, frame_type = struct.unpack('qqq', contents_batch[:24])
-                imps = np.frombuffer(contents_batch[24:],
-                                    dtype=np.float32).reshape((rows, cols))
+            with open(bin, "rb") as f:
+                contents_batch = f.read()
+                rows, cols, frame_type = struct.unpack(
+                    "qqq", contents_batch[:24]
+                )
+                imps = np.frombuffer(
+                    contents_batch[24:], dtype=np.float32
+                ).reshape((rows, cols))
                 imps_list.append(imps)
                 rows_list.append(rows)
                 col_list.append(cols)
@@ -108,35 +129,45 @@ def blockImportance(input, verbose, path, figure, raw, csv):
             click.echo(imps_list)
 
         if csv:
-            np.savetxt(str(folder_path +'/'+ csv), np.column_stack(
-                          (frame_type_list, mean_list)), delimiter=',',
-                      header = "Frame Type, Mean Importance ", fmt = "%d, %f")
+            np.savetxt(
+                str(folder_path + "/" + csv),
+                np.column_stack((frame_type_list, mean_list)),
+                delimiter=",",
+                header="Frame Type, Mean Importance ",
+                fmt="%d, %f",
+            )
 
     if verbose and path != None:
-        click.secho("Frame Type List: "+ str(frame_type_list))
-        click.secho("\n png list: "+ str(png_list), fg="green")
-        click.secho("\n bin list: "+ str(bin_list), fg="red")
-        click.secho("\n Total Count: "+ str(total_files))
-        click.secho("\n Mean Importance List: "+ str(mean_list))
+        click.secho("Frame Type List: " + str(frame_type_list))
+        click.secho("\n png list: " + str(png_list), fg="green")
+        click.secho("\n bin list: " + str(bin_list), fg="red")
+        click.secho("\n Total Count: " + str(total_files))
+        click.secho("\n Mean Importance List: " + str(mean_list))
 
     # Use a fixed scale where anything >= 10 cannot be distinguished
     # to allow visually comparing multiple pictures
-    max_imp = 10 ## Replace by `np.max(imps)` for relative scaling
+    max_imp = 10  ## Replace by `np.max(imps)` for relative scaling
     fig_list = []
     frame_size_multiplier = 4
-    mv_original_block_size = 8 // 2 # The importances are in 8×8 blocks, but we use half-resolution images.
+    mv_original_block_size = (
+        8 // 2
+    )  # The importances are in 8×8 blocks, but we use half-resolution images.
     mv_block_size = mv_original_block_size * frame_size_multiplier
 
     if path == None and figure:
         frame = Image.open(input[0])
-        frame = frame.resize((frame.width * frame_size_multiplier,
-                                        frame.height * frame_size_multiplier))
-        frame = frame.convert(mode='RGB')
-        draw = ImageDraw.Draw(frame, mode='RGBA')
+        frame = frame.resize(
+            (
+                frame.width * frame_size_multiplier,
+                frame.height * frame_size_multiplier,
+            )
+        )
+        frame = frame.convert(mode="RGB")
+        draw = ImageDraw.Draw(frame, mode="RGBA")
 
-         # Draw the grid.
+        # Draw the grid.
         for i in range(0, frame.width, mv_block_size):
-             draw.line(((i, 0), (i, frame.height)), fill=(0, 0, 0, 255))
+            draw.line(((i, 0), (i, frame.height)), fill=(0, 0, 0, 255))
         for i in range(0, frame.height, mv_block_size):
             draw.line(((0, i), (frame.width, i)), fill=(0, 0, 0, 255))
 
@@ -146,30 +177,48 @@ def blockImportance(input, verbose, path, figure, raw, csv):
                 for x in range(cols):
                     imp = imps[y, x]
                     top_left = (x * mv_block_size, y * mv_block_size)
-                    bottom_right = (top_left[0] + mv_block_size, top_left[1]
-                                                            + mv_block_size)
-                    draw.rectangle((top_left, bottom_right), fill=(int(imp /
-                                                    max_imp * 255), 0, 0, 128))
+                    bottom_right = (
+                        top_left[0] + mv_block_size,
+                        top_left[1] + mv_block_size,
+                    )
+                    draw.rectangle(
+                        (top_left, bottom_right),
+                        fill=(int(imp / max_imp * 255), 0, 0, 128),
+                    )
 
         fig = plt.figure(figsize=(frame.width, frame.height), dpi=1)
-        fig.figimage(frame, cmap='gray')
-        plt.savefig(folder_path+'/'+ splitext(os.path.basename(input[1]))[0] + '.png',
-                                                    bbox_inches='tight')
+        fig.figimage(frame, cmap="gray")
+        plt.savefig(
+            folder_path
+            + "/"
+            + splitext(os.path.basename(input[1]))[0]
+            + ".png",
+            bbox_inches="tight",
+        )
 
     elif figure:
-        for (png_batch, bin_batch, imps_batch, rows_batch, cols_batch) in  zip(
-                   png_list, bin_list, imps_list, rows_list, col_list):
+        for (png_batch, bin_batch, imps_batch, rows_batch, cols_batch) in zip(
+            png_list, bin_list, imps_list, rows_list, col_list
+        ):
             frame_batch = Image.open(png_batch)
-            frame_batch = frame_batch.resize((frame_batch.width *
-             frame_size_multiplier, frame_batch.height * frame_size_multiplier))
-            frame_batch = frame_batch.convert(mode='RGB')
-            draw = ImageDraw.Draw(frame_batch, mode='RGBA')
+            frame_batch = frame_batch.resize(
+                (
+                    frame_batch.width * frame_size_multiplier,
+                    frame_batch.height * frame_size_multiplier,
+                )
+            )
+            frame_batch = frame_batch.convert(mode="RGB")
+            draw = ImageDraw.Draw(frame_batch, mode="RGBA")
 
             # Draw the grid.
             for i in range(0, frame_batch.width, mv_block_size):
-                 draw.line(((i, 0), (i, frame_batch.height)), fill=(0, 0, 0, 255))
+                draw.line(
+                    ((i, 0), (i, frame_batch.height)), fill=(0, 0, 0, 255)
+                )
             for i in range(0, frame_batch.height, mv_block_size):
-                draw.line(((0, i), (frame_batch.width, i)), fill=(0, 0, 0, 255))
+                draw.line(
+                    ((0, i), (frame_batch.width, i)), fill=(0, 0, 0, 255)
+                )
 
             # Draw the importances.
             if max_imp > 0:
@@ -177,35 +226,55 @@ def blockImportance(input, verbose, path, figure, raw, csv):
                     for x in range(cols_batch):
                         imp = imps_batch[y, x]
                         top_left = (x * mv_block_size, y * mv_block_size)
-                        bottom_right = (top_left[0] + mv_block_size, top_left[1]
-                                                            + mv_block_size)
-                        draw.rectangle((top_left, bottom_right), fill=(int(imp
-                                                  / max_imp * 255), 0, 0, 128))
+                        bottom_right = (
+                            top_left[0] + mv_block_size,
+                            top_left[1] + mv_block_size,
+                        )
+                        draw.rectangle(
+                            (top_left, bottom_right),
+                            fill=(int(imp / max_imp * 255), 0, 0, 128),
+                        )
 
-            fig = plt.figure(figsize=(frame_batch.width, frame_batch.height),
-                                                                dpi=1)
-            fig.figimage(frame_batch, cmap='gray')
-            plt.savefig(folder_path + '/' + splitext(os.path.basename(bin_batch))[0]
-                            + ".png", bbox_inches='tight')
+            fig = plt.figure(
+                figsize=(frame_batch.width, frame_batch.height), dpi=1
+            )
+            fig.figimage(frame_batch, cmap="gray")
+            plt.savefig(
+                folder_path
+                + "/"
+                + splitext(os.path.basename(bin_batch))[0]
+                + ".png",
+                bbox_inches="tight",
+            )
             if verbose:
-                click.secho(str(folder_path + '/'+
-                        splitext(os.path.basename(bin_batch))[0] +".png"), fg="blue")
-
+                click.secho(
+                    str(
+                        folder_path
+                        + "/"
+                        + splitext(os.path.basename(bin_batch))[0]
+                        + ".png"
+                    ),
+                    fg="blue",
+                )
 
     if raw and figure:
-        click.secho("List of Figure Elements: "+str(fig_list))
+        click.secho("List of Figure Elements: " + str(fig_list))
 
     if verbose and figure:
         if path == None:
-            click.secho("File name is "+ splitext(input[1])[0]+ ".png",
-                                                            fg="blue")
-            print("Folder Path: "+ os.getcwd()+"/"+ folder_path)
+            click.secho(
+                "File name is " + splitext(input[1])[0] + ".png", fg="blue"
+            )
+            print("Folder Path: " + os.getcwd() + "/" + folder_path)
 
     elif verbose and csv:
-        click.secho("CSV Saved sucessfully: "+ folder_path + '/'+ csv +".", fg="blue")
+        click.secho(
+            "CSV Saved sucessfully: " + folder_path + "/" + csv + ".",
+            fg="blue",
+        )
 
     click.secho("Processing done.", fg="green")
 
+
 if __name__ == "__main__":
     blockImportance()
-
--- tools/submit_awcy.py
+++ tools/submit_awcy.py
@@ -2,23 +2,31 @@
 
 from __future__ import print_function
 
-import requests
 import argparse
 import os
 import subprocess
 import sys
 from datetime import datetime
 
-#our timestamping function, accurate to milliseconds
-#(remove [:-3] to display microseconds)
+import requests
+
+
+# our timestamping function, accurate to milliseconds
+# (remove [:-3] to display microseconds)
 def GetTime():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
-if "check_output" not in dir( subprocess ): # duck punch it in!
+
+if "check_output" not in dir(subprocess):  # duck punch it in!
+
     def f(*popenargs, **kwargs):
-        if 'stdout' in kwargs:
-            raise ValueError('stdout argument not allowed, it will be overridden.')
-        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        if "stdout" in kwargs:
+            raise ValueError(
+                "stdout argument not allowed, it will be overridden."
+            )
+        process = subprocess.Popen(
+            stdout=subprocess.PIPE, *popenargs, **kwargs
+        )
         output, unused_err = process.communicate()
         retcode = process.poll()
         if retcode:
@@ -27,59 +35,77 @@ if "check_output" not in dir( subprocess ): # duck punch it in!
                 cmd = popenargs[0]
             raise subprocess.CalledProcessError(retcode, cmd)
         return output
+
     subprocess.check_output = f
 
 key = None
 try:
-    with open('secret_key','r') as keyfile:
+    with open("secret_key", "r") as keyfile:
         key = keyfile.read().strip()
 except:
     print(GetTime(), "Could not open your secret_key file!")
-    print(GetTime(), "Please create a file called secret_key in the current directory")
+    print(
+        GetTime(),
+        "Please create a file called secret_key in the current directory",
+    )
     print(GetTime(), "containing your AreWeCompressedYet key.")
     sys.exit(1)
 
-parser = argparse.ArgumentParser(description='Submit test to arewecompressedyet.com')
-parser.add_argument('-branch',default=None)
-parser.add_argument('-prefix',default=None)
-parser.add_argument('-master',action='store_true',default=False)
-parser.add_argument('-set',default='objective-1-fast')
-parser.add_argument('-nick',default=None)
-parser.add_argument('-qualities',default=None,nargs='+',type=int)
-parser.add_argument('-extra_options',default='')
+parser = argparse.ArgumentParser(
+    description="Submit test to arewecompressedyet.com"
+)
+parser.add_argument("-branch", default=None)
+parser.add_argument("-prefix", default=None)
+parser.add_argument("-master", action="store_true", default=False)
+parser.add_argument("-set", default="objective-1-fast")
+parser.add_argument("-nick", default=None)
+parser.add_argument("-qualities", default=None, nargs="+", type=int)
+parser.add_argument("-extra_options", default="")
 args = parser.parse_args()
 
 if args.branch is None:
     try:
-        args.branch = subprocess.check_output('git symbolic-ref -q --short HEAD',shell=True).strip().decode('utf-8')
+        args.branch = (
+            subprocess.check_output(
+                "git symbolic-ref -q --short HEAD", shell=True
+            )
+            .strip()
+            .decode("utf-8")
+        )
     except:
         args.branch = None
 
 if args.prefix is None:
     args.prefix = args.branch
 
-commit = subprocess.check_output('git rev-parse HEAD',shell=True).strip()
-short = subprocess.check_output('git rev-parse --short HEAD',shell=True).strip().decode("utf-8")
+commit = subprocess.check_output("git rev-parse HEAD", shell=True).strip()
+short = (
+    subprocess.check_output("git rev-parse --short HEAD", shell=True)
+    .strip()
+    .decode("utf-8")
+)
 date = GetTime().strip()
-date_short = date.split()[0]+'_'+date.split()[1].split('.')[0].replace(':', '')
+date_short = (
+    date.split()[0] + "_" + date.split()[1].split(".")[0].replace(":", "")
+)
 prefix = args.prefix
 is_master = args.master
-run_id = prefix+'-'+date_short+'-'+short
+run_id = prefix + "-" + date_short + "-" + short
 
 params = {
-    'run_id': run_id,
-    'commit': commit,
-    'master': is_master,
-    'key': key,
-    'task': args.set,
-    'codec': 'rav1e',
-    'nick': args.nick,
-    'extra_options': args.extra_options,
+    "run_id": run_id,
+    "commit": commit,
+    "master": is_master,
+    "key": key,
+    "task": args.set,
+    "codec": "rav1e",
+    "nick": args.nick,
+    "extra_options": args.extra_options,
 }
 
 if args.qualities is not None:
-    params['qualities'] = ' '.join(map(str, args.qualities))
+    params["qualities"] = " ".join(map(str, args.qualities))
 
-print(GetTime(), 'Creating run '+run_id)
+print(GetTime(), "Creating run " + run_id)
 r = requests.post("https://beta.arewecompressedyet.com/submit/job", params)
 print(GetTime(), r)
