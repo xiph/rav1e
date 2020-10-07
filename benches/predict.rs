988778418984123848e952666ad5d6cc7e8b6cab
Jon Moroney <darakian@gmail.com>
2020-10-07 11:00:34 -0700

    Update for 2018 copyrights
    Full bash script for future use
    ```
    for file in $(git log --since=2020-01-01 --name-only --oneline | rg -v "^[a-zA-Z0-9]{8}")
    do
        if  [ ! -f "$file" ]; then
            continue
        fi
        if grep "\-201[0-9], The rav1e contributors" $file; then
            gsed -i 's/-201[0-9], The rav1e contributors/-2020, The rav1e contributors/' $file
        fi
    done
    ```

--- benches/predict.rs
+++ benches/predict.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, The rav1e contributors. All rights reserved
+// Copyright (c) 2017-2020, The rav1e contributors. All rights reserved
 //
 // This source code is subject to the terms of the BSD 2 Clause License and
 // the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
