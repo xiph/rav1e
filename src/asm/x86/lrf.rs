814104873d55250e7b3775bc71f71dfa6b123fe0
Jon Moroney <darakian@gmail.com>
2020-10-07 11:10:11 -0700

    Update files touched this year and make copyright a range
    This commit updates copyrights and makes them a range
    ```
    for file in $(git log --since=2020-01-01 --name-only --oneline | rg -v "^[a-zA-Z0-9]{8}")
    do
        if  [ ! -f "$file" ]; then
            continue
        fi
        if grep "Copyright \(c\) 2019, The rav1e contributors." $file; then
            echo $file
            sed -i 's/Copyright (c) 2019, The rav1e/Copyright (c) 2019-2020, The rav1e/' $file
        fi
    done
    ```

--- src/asm/x86/lrf.rs
+++ src/asm/x86/lrf.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, The rav1e contributors. All rights reserved
+// Copyright (c) 2019-2020, The rav1e contributors. All rights reserved
 //
 // This source code is subject to the terms of the BSD 2 Clause License and
 // the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
