b0a8e8060df7b9f42c02927776280c06ea336365
Monty Montgomery <xiphmont@gmail.com>
2020-10-07 15:44:10 -0400

    Add first of ARM 64 bit assembly for CDEF
    Add long included but unused dav1d 0.7 ARM 64 bit cdef assembly to
    active build, enable first of the CDEF funcitonality (cdef_find_dir
    for 8 and 16bpp)

--- src/cdef.rs
+++ src/cdef.rs
@@ -21,6 +21,8 @@ use std::cmp;
 cfg_if::cfg_if! {
   if #[cfg(nasm_x86_64)] {
     pub(crate) use crate::asm::x86::cdef::*;
+  } else if #[cfg(asm_neon)] {
+    pub(crate) use crate::asm::aarch64::cdef::*;
   } else {
     pub(crate) use self::rust::*;
   }
