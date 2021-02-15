f2ecbe519fb5f89f8131fd8cd2fa8e95a32b13ad
David Michael Barr <b@rr-dav.id.au>
2021-02-15 15:54:37 +0900

    CI: Update libaom to 2.0.2-dmo0~bpo10+1

--- .travis/install-aom.sh
+++ .travis/install-aom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-AOM_VERSION="2.0.1-dmo0~bpo10+1"
+AOM_VERSION="2.0.2-dmo0~bpo10+1"
 PKG_URL="https://www.deb-multimedia.org/pool/main/a/aom-dmo"
 ARCH="arm64"
 
@@ -13,8 +13,8 @@ curl -O "${PKG_URL}/libaom-dev_${AOM_VERSION}_${ARCH}.deb" \
      -O "${PKG_URL}/libaom2_${AOM_VERSION}_${ARCH}.deb"
 
 sha256sum --check --ignore-missing <<EOF
-26fcaf306ab6ca528fb26352460a00aa60b4f0d2cd1ba6c1de4af41352414c71  libaom2_${AOM_VERSION}_${ARCH}.deb
-612b6f86a8dff9b6a4cd33216cdaf298605b6818159f8a9a056e7a73ce935481  libaom-dev_${AOM_VERSION}_${ARCH}.deb
+2352aa82e15f3936c2dd21d3aee6633b8338e96c09b38b2912aa2c1555a758a2  libaom2_${AOM_VERSION}_${ARCH}.deb
+80e7c9ea59f4fc9ac6518e071ee8f86ba2ccec2d5500ea222982fa6dfa21356c  libaom-dev_${AOM_VERSION}_${ARCH}.deb
 EOF
 
 sudo dpkg -i "libaom2_${AOM_VERSION}_${ARCH}.deb" \
