02106e0bc2019723545cd65f08eaaef25a9e434d
Vibhoothi <vibhoothiiaanand@gmail.com>
2021-04-08 13:13:15 +0100

    CI: Update to libaom3 for both travis and GitHub Actions

--- .travis/install-aom.sh
+++ .travis/install-aom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-AOM_VERSION="2.0.2-dmo0~bpo10+1"
+AOM_VERSION="3.0.0-dmo0~bpo10+1"
 PKG_URL="https://www.deb-multimedia.org/pool/main/a/aom-dmo"
 ARCH="arm64"
 
@@ -10,12 +10,12 @@ cd "$DEPS_DIR"
 [ -f "libaom-dev_${AOM_VERSION}_${ARCH}.deb" ] &&
 [ -f "libaom2_${AOM_VERSION}_${ARCH}.deb" ] ||
 curl -O "${PKG_URL}/libaom-dev_${AOM_VERSION}_${ARCH}.deb" \
-     -O "${PKG_URL}/libaom2_${AOM_VERSION}_${ARCH}.deb"
+     -O "${PKG_URL}/libaom3_${AOM_VERSION}_${ARCH}.deb"
 
 sha256sum --check --ignore-missing <<EOF
-2352aa82e15f3936c2dd21d3aee6633b8338e96c09b38b2912aa2c1555a758a2  libaom2_${AOM_VERSION}_${ARCH}.deb
-80e7c9ea59f4fc9ac6518e071ee8f86ba2ccec2d5500ea222982fa6dfa21356c  libaom-dev_${AOM_VERSION}_${ARCH}.deb
+1a9a7d34175871d96afa98981b1f4a60d84a420ee9cc3b1f61c7d2f1bdcb0ae5  libaom3_${AOM_VERSION}_${ARCH}.deb
+aa0f6dd3ec62f2682d1d2322e305076e951b3118aee2d0adc5add7697ec88ff7  libaom-dev_${AOM_VERSION}_${ARCH}.deb
 EOF
 
-sudo dpkg -i "libaom2_${AOM_VERSION}_${ARCH}.deb" \
+sudo dpkg -i "libaom3_${AOM_VERSION}_${ARCH}.deb" \
              "libaom-dev_${AOM_VERSION}_${ARCH}.deb"
