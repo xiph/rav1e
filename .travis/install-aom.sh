7749085a1b9edf033a07b0161c0e92d2e803cd91
David Michael Barr <b@rr-dav.id.au>
2021-05-07 23:39:23 +0900

    CI: Update libaom to 3.1.0-dmo0~bpo10+1

--- .travis/install-aom.sh
+++ .travis/install-aom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-AOM_VERSION="3.0.0-dmo0~bpo10+1"
+AOM_VERSION="3.1.0-dmo0~bpo10+1"
 PKG_URL="https://www.deb-multimedia.org/pool/main/a/aom-dmo"
 ARCH="arm64"
 
@@ -13,8 +13,8 @@ curl -O "${PKG_URL}/libaom-dev_${AOM_VERSION}_${ARCH}.deb" \
      -O "${PKG_URL}/libaom3_${AOM_VERSION}_${ARCH}.deb"
 
 sha256sum --check --ignore-missing <<EOF
-1a9a7d34175871d96afa98981b1f4a60d84a420ee9cc3b1f61c7d2f1bdcb0ae5  libaom3_${AOM_VERSION}_${ARCH}.deb
-aa0f6dd3ec62f2682d1d2322e305076e951b3118aee2d0adc5add7697ec88ff7  libaom-dev_${AOM_VERSION}_${ARCH}.deb
+1846784bceba7d3c46c9672872c25292001aebd488a2035df49c9cae9a674c2a  libaom3_${AOM_VERSION}_${ARCH}.deb
+1c2e509cd7fbb30304c2311f46509a68ebf939adaf9f7fd81cdc466866d06c05  libaom-dev_${AOM_VERSION}_${ARCH}.deb
 EOF
 
 sudo dpkg -i "libaom3_${AOM_VERSION}_${ARCH}.deb" \
