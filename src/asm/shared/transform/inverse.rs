57f8ea4e3b087c3a76f523c35014f5846b39a52d
Josh Holmer <jholmer.in@gmail.com>
2021-05-13 01:14:34 -0400

    Enable 10-bit inverse transform ASM

--- src/asm/shared/transform/inverse.rs
+++ src/asm/shared/transform/inverse.rs
@@ -14,7 +14,6 @@ use crate::util::*;
 pub type InvTxfmFunc =
   unsafe extern fn(*mut u8, libc::ptrdiff_t, *mut i16, i32);
 
-#[cfg(asm_neon)]
 pub type InvTxfmHBDFunc =
   unsafe extern fn(*mut u16, libc::ptrdiff_t, *mut i16, i32);
 
@@ -46,7 +45,6 @@ pub fn call_inverse_func<T: Pixel>(
   }
 }
 
-#[cfg(asm_neon)]
 pub fn call_inverse_hbd_func<T: Pixel>(
   func: InvTxfmHBDFunc, input: &[T::Coeff],
   output: &mut PlaneRegionMut<'_, T>, eob: usize, width: usize, height: usize,
