d2bc4d20db705b364f8bb7ed0da5cfabf8a985e2
Josh Holmer <jholmer.in@gmail.com>
2022-03-25 20:28:18 -0400

    Break the scenechange module into smaller parts

--- src/scenechange/mod.rs
+++ src/scenechange/mod.rs
@@ -7,32 +7,24 @@
 // Media Patent License 1.0 was not distributed with this source code in the
 // PATENTS file, you can obtain it at www.aomedia.org/license/patent.
 
-use crate::api::lookahead::*;
+mod fast;
+mod standard;
+
 use crate::api::{EncoderConfig, SceneDetectionSpeed};
 use crate::cpu_features::CpuFeatureLevel;
 use crate::encoder::Sequence;
 use crate::frame::*;
 use crate::me::FrameMEStats;
 use crate::partition::REF_FRAMES;
-use crate::sad_row;
 use crate::util::Pixel;
 use rust_hawktracer::*;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::{cmp, u64};
-use v_frame::math::Fixed;
 
-// The fast implementation is based on a Python implementation at
-// https://pyscenedetect.readthedocs.io/en/latest/reference/detection-methods/.
-// The Python implementation uses HSV values and a threshold of 30. Comparing the
-// YUV values was sufficient in most cases, and avoided a more costly YUV->RGB->HSV
-// conversion, but the deltas needed to be scaled down. The deltas for keyframes
-// in YUV were about 1/3 to 1/2 of what they were in HSV, but non-keyframes were
-// very unlikely to have a delta greater than 3 in YUV, whereas they may reach into
-// the double digits in HSV.
-//
-// Experiments have shown that these thresholds are optimal.
-const FAST_THRESHOLD: f64 = 18.0;
+use self::fast::{detect_scale_factor, FAST_THRESHOLD};
+
+/// Experiments have determined this to be an optimal threshold
 const IMP_BLOCK_DIFF_THRESHOLD: f64 = 7.0;
 
 /// Runs keyframe detection on frames from the lookahead queue.
@@ -88,7 +80,7 @@ impl<T: Pixel> SceneChangeDetector<T> {
       encoder_config.speed_settings.scene_detection_mode
     };
 
-    // Scale factor for fast and medium scene detection
+    // Scale factor for fast scene detection
     let scale_factor = detect_scale_factor(&sequence, speed_mode);
 
     // Set lookahead offset to 5 if normal lookahead available
@@ -355,212 +347,6 @@ impl<T: Pixel> SceneChangeDetector<T> {
 
     (cost >= score.threshold, score)
   }
-
-  /// The fast algorithm detects fast cuts using a raw difference
-  /// in pixel values between the scaled frames.
-  #[hawktracer(fast_scenecut)]
-  fn fast_scenecut(
-    &mut self, frame1: Arc<Frame<T>>, frame2: Arc<Frame<T>>,
-  ) -> ScenecutResult {
-    if self.scale_factor == 1 {
-      if let Some(frame_buffer) = self.frame_ref_buffer.as_deref_mut() {
-        frame_buffer.swap(0, 1);
-        frame_buffer[1] = frame2;
-      } else {
-        self.frame_ref_buffer = Some(Box::new([frame1, frame2]));
-      }
-
-      if let Some(frame_buffer) = self.frame_ref_buffer.as_deref() {
-        let delta = self.delta_in_planes(
-          &frame_buffer[0].planes[0],
-          &frame_buffer[1].planes[0],
-        );
-
-        ScenecutResult {
-          threshold: self.threshold as f64,
-          inter_cost: delta as f64,
-          imp_block_cost: delta as f64,
-          backward_adjusted_cost: delta as f64,
-          forward_adjusted_cost: delta as f64,
-        }
-      } else {
-        unreachable!()
-      }
-    } else {
-      // downscale both frames for faster comparison
-      if let Some((frame_buffer, is_initialized)) =
-        &mut self.downscaled_frame_buffer
-      {
-        let frame_buffer = &mut **frame_buffer;
-        if *is_initialized {
-          frame_buffer.swap(0, 1);
-          frame2.planes[0]
-            .downscale_in_place(self.scale_factor, &mut frame_buffer[1]);
-        } else {
-          // both frames are in an irrelevant and invalid state, so we have to reinitialize
-          // them, but we can reuse their allocations
-          frame1.planes[0]
-            .downscale_in_place(self.scale_factor, &mut frame_buffer[0]);
-          frame2.planes[0]
-            .downscale_in_place(self.scale_factor, &mut frame_buffer[1]);
-          *is_initialized = true;
-        }
-      } else {
-        self.downscaled_frame_buffer = Some((
-          Box::new([
-            frame1.planes[0].downscale(self.scale_factor),
-            frame2.planes[0].downscale(self.scale_factor),
-          ]),
-          true, // the frame buffer is initialized and in a valid state
-        ));
-      }
-
-      if let Some((frame_buffer, _)) = &self.downscaled_frame_buffer {
-        let frame_buffer = &**frame_buffer;
-        let delta = self.delta_in_planes(&frame_buffer[0], &frame_buffer[1]);
-
-        ScenecutResult {
-          threshold: self.threshold as f64,
-          inter_cost: delta as f64,
-          imp_block_cost: delta as f64,
-          forward_adjusted_cost: delta as f64,
-          backward_adjusted_cost: delta as f64,
-        }
-      } else {
-        unreachable!()
-      }
-    }
-  }
-
-  /// Run a comparison between two frames to determine if they qualify for a scenecut.
-  ///
-  /// We gather both intra and inter costs for the frames,
-  /// as well as an importance-block-based difference,
-  /// and use all three metrics.
-  #[hawktracer(cost_scenecut)]
-  fn cost_scenecut(
-    &mut self, frame1: Arc<Frame<T>>, frame2: Arc<Frame<T>>,
-    input_frameno: u64,
-  ) -> ScenecutResult {
-    let frame2_inter_ref = Arc::clone(&frame2);
-    let frame1_imp_ref = Arc::clone(&frame1);
-    let frame2_imp_ref = Arc::clone(&frame2);
-
-    let mut intra_cost = 0.0;
-    let mut mv_inter_cost = 0.0;
-    let mut imp_block_cost = 0.0;
-
-    let cols = 2 * self.encoder_config.width.align_power_of_two_and_shift(3);
-    let rows = 2 * self.encoder_config.height.align_power_of_two_and_shift(3);
-
-    let buffer = if let Some(buffer) = &self.frame_me_stats_buffer {
-      Arc::clone(buffer)
-    } else {
-      let frame_me_stats = FrameMEStats::new_arc_array(cols, rows);
-      let clone = Arc::clone(&frame_me_stats);
-      self.frame_me_stats_buffer = Some(frame_me_stats);
-      clone
-    };
-
-    crate::rayon::scope(|s| {
-      s.spawn(|_| {
-        let intra_costs =
-          self.intra_costs.entry(input_frameno).or_insert_with(|| {
-            estimate_intra_costs(
-              &*frame2,
-              self.bit_depth,
-              self.cpu_feature_level,
-            )
-          });
-        intra_cost = intra_costs.iter().map(|&cost| cost as u64).sum::<u64>()
-          as f64
-          / intra_costs.len() as f64;
-        // If we're not using temporal RDO, we won't need these costs later,
-        // so remove them from the cache to avoid a memory leak
-        if !self.encoder_config.temporal_rdo() {
-          self.intra_costs.remove(&input_frameno);
-        };
-      });
-      s.spawn(|_| {
-        mv_inter_cost = estimate_inter_costs(
-          frame2_inter_ref,
-          frame1,
-          self.bit_depth,
-          self.encoder_config,
-          self.sequence.clone(),
-          buffer,
-        );
-      });
-      s.spawn(|_| {
-        imp_block_cost =
-          estimate_importance_block_difference(frame2_imp_ref, frame1_imp_ref);
-      });
-    });
-
-    // `BIAS` determines how likely we are
-    // to choose a keyframe, between 0.0-1.0.
-    // Higher values mean we are more likely to choose a keyframe.
-    // This value was chosen based on trials using the new
-    // adaptive scenecut code.
-    const BIAS: f64 = 0.7;
-    let threshold = intra_cost * (1.0 - BIAS);
-
-    ScenecutResult {
-      inter_cost: mv_inter_cost,
-      imp_block_cost,
-      threshold,
-      backward_adjusted_cost: 0.0,
-      forward_adjusted_cost: 0.0,
-    }
-  }
-
-  /// Calculates the average sum of absolute difference (SAD) per pixel between 2 planes
-  #[hawktracer(delta_in_planes)]
-  fn delta_in_planes(&self, plane1: &Plane<T>, plane2: &Plane<T>) -> f64 {
-    let mut delta = 0;
-
-    let lines = plane1.rows_iter().zip(plane2.rows_iter());
-
-    for (l1, l2) in lines {
-      let l1 = l1.get(..plane1.cfg.width).unwrap_or(l1);
-      let l2 = l2.get(..plane1.cfg.width).unwrap_or(l2);
-      delta += sad_row::sad_row(l1, l2, self.cpu_feature_level);
-    }
-    delta as f64 / self.pixels as f64
-  }
-}
-
-/// Scaling factor for frame in scene detection
-fn detect_scale_factor(
-  sequence: &Arc<Sequence>, speed_mode: SceneDetectionSpeed,
-) -> usize {
-  let small_edge =
-    cmp::min(sequence.max_frame_height, sequence.max_frame_width) as usize;
-  let scale_factor = if speed_mode == SceneDetectionSpeed::Fast {
-    match small_edge {
-      0..=240 => 1,
-      241..=480 => 2,
-      481..=720 => 4,
-      721..=1080 => 8,
-      1081..=1600 => 16,
-      1601..=usize::MAX => 32,
-      _ => 1,
-    }
-  } else {
-    1
-  };
-
-  if scale_factor != 1 {
-    debug!(
-      "Scene detection scale factor {}, [{},{}] -> [{},{}]",
-      scale_factor,
-      sequence.max_frame_width,
-      sequence.max_frame_height,
-      sequence.max_frame_width as usize / scale_factor,
-      sequence.max_frame_height as usize / scale_factor
-    );
-  }
-  scale_factor
 }
 
 #[derive(Debug, Clone, Copy)]
