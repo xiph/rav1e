9da184c0147221eb88a27ad4f65f87f76143bdf0
David Michael Barr <b@rr-dav.id.au>
2023-04-15 21:38:30 +0900

    Integrate CfL AC aarch64 assembly functions

--- src/asm/aarch64/predict.rs
+++ src/asm/aarch64/predict.rs
@@ -9,16 +9,55 @@
 
 use crate::context::MAX_TX_SIZE;
 use crate::cpu_features::CpuFeatureLevel;
+use crate::partition::BlockSize;
 use crate::predict::{
   rust, IntraEdgeFilterParameters, PredictionMode, PredictionVariant,
 };
-use crate::tiling::PlaneRegionMut;
+use crate::tiling::{PlaneRegion, PlaneRegionMut};
 use crate::transform::TxSize;
 use crate::util::Aligned;
 use crate::{Pixel, PixelType};
 use libc;
 
-pub(crate) use crate::predict::rust::pred_cfl_ac;
+macro_rules! decl_cfl_ac_fn {
+  ($($f:ident),+) => {
+    extern {
+      $(
+        fn $f(
+          ac: *mut i16, src: *const u8, stride: libc::ptrdiff_t,
+          w_pad: libc::c_int, h_pad: libc::c_int,
+          width: libc::c_int, height: libc::c_int,
+        );
+      )*
+    }
+  };
+}
+
+decl_cfl_ac_fn! {
+  rav1e_ipred_cfl_ac_420_8bpc_neon,
+  rav1e_ipred_cfl_ac_422_8bpc_neon,
+  rav1e_ipred_cfl_ac_444_8bpc_neon
+}
+
+macro_rules! decl_cfl_ac_hbd_fn {
+  ($($f:ident),+) => {
+    extern {
+      $(
+        fn $f(
+          ac: *mut i16, src: *const u16, stride: libc::ptrdiff_t,
+          w_pad: libc::c_int, h_pad: libc::c_int,
+          width: libc::c_int, height: libc::c_int,
+        );
+      )*
+    }
+  };
+}
+
+decl_cfl_ac_hbd_fn! {
+  rav1e_ipred_cfl_ac_420_16bpc_neon,
+  rav1e_ipred_cfl_ac_422_16bpc_neon,
+  rav1e_ipred_cfl_ac_444_16bpc_neon
+}
 
 macro_rules! decl_angular_ipred_fn {
   ($($f:ident),+) => {
@@ -240,6 +279,47 @@ pub fn dispatch_predict_intra<T: Pixel>(
         _ => call_rust(dst),
       },
       _ => call_rust(dst),
+    }
+  }
+}
+
+#[inline(always)]
+pub(crate) fn pred_cfl_ac<T: Pixel, const XDEC: usize, const YDEC: usize>(
+  ac: &mut [i16], luma: &PlaneRegion<'_, T>, bsize: BlockSize, w_pad: usize,
+  h_pad: usize, cpu: CpuFeatureLevel,
+) {
+  if cpu < CpuFeatureLevel::NEON {
+    return rust::pred_cfl_ac::<T, XDEC, YDEC>(
+      ac, luma, bsize, w_pad, h_pad, cpu,
+    );
+  }
+
+  let stride = T::to_asm_stride(luma.plane_cfg.stride) as libc::ptrdiff_t;
+  let w = bsize.width() as libc::c_int;
+  let h = bsize.height() as libc::c_int;
+  let w_pad = w_pad as libc::c_int;
+  let h_pad = h_pad as libc::c_int;
+
+  // SAFETY: Calls Assembly code.
+  unsafe {
+    let ac_ptr = ac.as_mut_ptr();
+    match T::type_enum() {
+      PixelType::U8 => {
+        let luma_ptr = luma.data_ptr() as *const u8;
+        (match (XDEC, YDEC) {
+          (0, 0) => rav1e_ipred_cfl_ac_444_8bpc_neon,
+          (1, 0) => rav1e_ipred_cfl_ac_422_8bpc_neon,
+          _ => rav1e_ipred_cfl_ac_420_8bpc_neon,
+        })(ac_ptr, luma_ptr, stride, w_pad, h_pad, w, h)
+      }
+      PixelType::U16 => {
+        let luma_ptr = luma.data_ptr() as *const u16;
+        (match (XDEC, YDEC) {
+          (0, 0) => rav1e_ipred_cfl_ac_444_16bpc_neon,
+          (1, 0) => rav1e_ipred_cfl_ac_422_16bpc_neon,
+          _ => rav1e_ipred_cfl_ac_420_16bpc_neon,
+        })(ac_ptr, luma_ptr, stride, w_pad, h_pad, w, h)
+      }
     }
   }
 }
