e8c9d493dfa46ea490c6e0332ca89e285465d4f9
Frankie Dintino <fdintino@gmail.com>
2023-04-17 16:53:27 -0400

    Change cbuild to cinstall

--- cross/entrypoint-build-libs.sh
+++ cross/entrypoint-build-libs.sh
@@ -2,7 +2,7 @@
 
 for arg in "$@"; do
   arg=$(echo $arg | perl -pne \
-    's/(?:(?<=\s)|^)build(?=\s)/cbuild --library-type staticlib --library-type cdylib/')
+    's/(?:(?<=\s)|^)build(?=\s)/cinstall --library-type staticlib --library-type cdylib --prefix dist/')
   set -- "$@" "$arg"
   shift
 done
