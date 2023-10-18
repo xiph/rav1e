18daa517cd49e1c5a02f83cdaacaac599fc6e752
David Michael Barr <b@rr-dav.id.au>
2023-10-18 17:42:11 +0900

    Add forward and inverse Walsh-Hadamard transforms

--- src/asm/aarch64/transform/inverse.rs
+++ src/asm/aarch64/transform/inverse.rs
@@ -16,6 +16,8 @@ use crate::{Pixel, PixelType};
 use crate::asm::shared::transform::inverse::*;
 use crate::asm::shared::transform::*;
 
+pub use crate::transform::inverse::rust::inverse_transform_add_lossless;
+
 pub fn inverse_transform_add<T: Pixel>(
   input: &[T::Coeff], output: &mut PlaneRegionMut<'_, T>, eob: usize,
   tx_size: TxSize, tx_type: TxType, bd: usize, cpu: CpuFeatureLevel,
