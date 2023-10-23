2442c4641bc3d2811ec3e5c34003a39381ef1b59
David Michael Barr <b@rr-dav.id.au>
2023-10-23 14:53:56 +0900

    Consistent setup for round trip and inverse transform tests

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -461,9 +461,8 @@ mod test {
     let coeff_area: usize = av1_get_coded_tx_size(tx_size).area();
     let mut src_storage = [T::cast_from(0); 64 * 64];
     let src = &mut src_storage[..tx_size.area()];
-    // dynamic allocation: test
     let mut dst = Plane::from_slice(
-      &vec![T::cast_from(0); tx_size.area()],
+      &[T::zero(); 64 * 64][..tx_size.area()],
       tx_size.width(),
     );
     let mut res_storage = [0i16; 64 * 64];
@@ -500,8 +499,7 @@ mod test {
 
     let mut src_storage = [T::cast_from(0); 4 * 4];
     let src = &mut src_storage[..];
-    // dynamic allocation: test
-    let mut dst = Plane::from_slice(&vec![T::cast_from(0); 4 * 4], 4);
+    let mut dst = Plane::from_slice(&[T::cast_from(0); 4 * 4], 4);
     let mut res_storage = [0i16; 4 * 4];
     let res = &mut res_storage[..];
     let mut freq_storage = [T::Coeff::cast_from(0); 4 * 4];
