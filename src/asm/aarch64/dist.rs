ab3a849278055a59288acf21719c32d58243c496
David Michael Barr <b@rr-dav.id.au>
2023-11-01 23:27:04 +0900

    Implement 8x8 SATD with NEON intrinsics

--- src/asm/aarch64/dist.rs
+++ src/asm/aarch64/dist.rs
@@ -108,6 +108,101 @@ pub fn get_sad<T: Pixel>(
   dist
 }
 
+#[no_mangle]
+unsafe extern fn rav1e_satd8x8_neon(
+  src: *const u8, src_stride: isize, dst: *const u8, dst_stride: isize,
+) -> u32 {
+  use core::arch::aarch64::*;
+
+  let load_row = |src: *const u8, dst: *const u8| -> int16x8_t {
+    vreinterpretq_s16_u16(vsubl_u8(vld1_u8(src), vld1_u8(dst)))
+  };
+  let butterfly = |a: int16x8_t, b: int16x8_t| -> int16x8x2_t {
+    int16x8x2_t(vaddq_s16(a, b), vsubq_s16(a, b))
+  };
+  let zip1 = |v: int16x8x2_t| -> int16x8x2_t {
+    int16x8x2_t(vzip1q_s16(v.0, v.1), vzip2q_s16(v.0, v.1))
+  };
+  let zip2 = |v: int16x8x2_t| -> int16x8x2_t {
+    let v =
+      int32x4x2_t(vreinterpretq_s32_s16(v.0), vreinterpretq_s32_s16(v.1));
+    let v = int32x4x2_t(vzip1q_s32(v.0, v.1), vzip2q_s32(v.0, v.1));
+    int16x8x2_t(vreinterpretq_s16_s32(v.0), vreinterpretq_s16_s32(v.1))
+  };
+  let zip4 = |v: int16x8x2_t| -> int16x8x2_t {
+    let v =
+      int64x2x2_t(vreinterpretq_s64_s16(v.0), vreinterpretq_s64_s16(v.1));
+    let v = int64x2x2_t(vzip1q_s64(v.0, v.1), vzip2q_s64(v.0, v.1));
+    int16x8x2_t(vreinterpretq_s16_s64(v.0), vreinterpretq_s16_s64(v.1))
+  };
+
+  let (src_stride2, dst_stride2) = (src_stride << 1, dst_stride << 1);
+  let int16x8x2_t(r0, r1) = zip1(butterfly(
+    load_row(src, dst),
+    load_row(src.offset(src_stride), dst.offset(dst_stride)),
+  ));
+  let (src, dst) = (src.offset(src_stride2), dst.offset(dst_stride2));
+  let int16x8x2_t(r2, r3) = zip1(butterfly(
+    load_row(src, dst),
+    load_row(src.offset(src_stride), dst.offset(dst_stride)),
+  ));
+  let (src, dst) = (src.offset(src_stride2), dst.offset(dst_stride2));
+  let int16x8x2_t(r4, r5) = zip1(butterfly(
+    load_row(src, dst),
+    load_row(src.offset(src_stride), dst.offset(dst_stride)),
+  ));
+  let (src, dst) = (src.offset(src_stride2), dst.offset(dst_stride2));
+  let int16x8x2_t(r6, r7) = zip1(butterfly(
+    load_row(src, dst),
+    load_row(src.offset(src_stride), dst.offset(dst_stride)),
+  ));
+
+  let int16x8x2_t(r0, r2) = zip2(butterfly(r0, r2));
+  let int16x8x2_t(r1, r3) = zip2(butterfly(r1, r3));
+  let int16x8x2_t(r4, r6) = zip2(butterfly(r4, r6));
+  let int16x8x2_t(r5, r7) = zip2(butterfly(r5, r7));
+
+  let int16x8x2_t(r0, r4) = zip4(butterfly(r0, r4));
+  let int16x8x2_t(r1, r5) = zip4(butterfly(r1, r5));
+  let int16x8x2_t(r2, r6) = zip4(butterfly(r2, r6));
+  let int16x8x2_t(r3, r7) = zip4(butterfly(r3, r7));
+
+  let int16x8x2_t(r0, r1) = butterfly(r0, r1);
+  let int16x8x2_t(r2, r3) = butterfly(r2, r3);
+  let int16x8x2_t(r4, r5) = butterfly(r4, r5);
+  let int16x8x2_t(r6, r7) = butterfly(r6, r7);
+
+  let int16x8x2_t(r0, r2) = butterfly(r0, r2);
+  let int16x8x2_t(r1, r3) = butterfly(r1, r3);
+  let int16x8x2_t(r4, r6) = butterfly(r4, r6);
+  let int16x8x2_t(r5, r7) = butterfly(r5, r7);
+
+  let int16x8x2_t(r0, r4) = butterfly(r0, r4);
+  let int16x8x2_t(r1, r5) = butterfly(r1, r5);
+  let int16x8x2_t(r2, r6) = butterfly(r2, r6);
+  let int16x8x2_t(r3, r7) = butterfly(r3, r7);
+
+  let r0 = vabsq_s16(r0);
+  let r1 = vabsq_s16(r1);
+  let r2 = vabsq_s16(r2);
+  let r3 = vabsq_s16(r3);
+  let r4 = vabsq_s16(r4);
+  let r5 = vabsq_s16(r5);
+  let r6 = vabsq_s16(r6);
+  let r7 = vabsq_s16(r7);
+
+  let (t0, t1) = (vmovl_s16(vget_low_s16(r0)), vmovl_s16(vget_high_s16(r0)));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r1)), vaddw_high_s16(t1, r1));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r2)), vaddw_high_s16(t1, r2));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r3)), vaddw_high_s16(t1, r3));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r4)), vaddw_high_s16(t1, r4));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r5)), vaddw_high_s16(t1, r5));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r6)), vaddw_high_s16(t1, r6));
+  let (t0, t1) = (vaddw_s16(t0, vget_low_s16(r7)), vaddw_high_s16(t1, r7));
+
+  (vaddvq_s32(vaddq_s32(t0, t1)) as u32 + 4) >> 3
+}
+
 #[inline(always)]
 #[allow(clippy::let_and_return)]
 pub fn get_satd<T: Pixel>(
@@ -192,6 +287,8 @@ static SATD_FNS_NEON: [Option<SatdFn>; DIST_FNS_LENGTH] = {
   out[BLOCK_4X16 as usize] = Some(rav1e_satd4x16_neon);
   out[BLOCK_8X4 as usize] = Some(rav1e_satd8x4_neon);
   out[BLOCK_16X4 as usize] = Some(rav1e_satd16x4_neon);
+
+  out[BLOCK_8X8 as usize] = Some(rav1e_satd8x8_neon);
 
   out
 };
@@ -278,6 +375,19 @@ mod test {
     (128, 64),
     (128, 128),
     sad,
+    8,
+    neon,
+    "neon"
+  );
+
+  test_dist_fns!(
+    (4, 4),
+    (4, 8),
+    (4, 16),
+    (8, 4),
+    (8, 8),
+    (16, 4),
+    satd,
     8,
     neon,
     "neon"
