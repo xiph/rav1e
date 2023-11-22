d29d9096dbae91b217700faf50498d95bd57ab28
David Michael Barr <b@rr-dav.id.au>
2023-11-22 14:53:09 +0900

    cdef_dist_kernel: Move assembly tests to shared module

--- src/asm/shared/dist/mod.rs
+++ src/asm/shared/dist/mod.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020, The rav1e contributors. All rights reserved
+// Copyright (c) 2022, The rav1e contributors. All rights reserved
 //
 // This source code is subject to the terms of the BSD 2 Clause License and
 // the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
@@ -7,6 +7,4 @@
 // Media Patent License 1.0 was not distributed with this source code in the
 // PATENTS file, you can obtain it at www.aomedia.org/license/patent.
 
-pub mod dist;
-pub mod predict;
-pub mod transform;
+pub mod cdef_dist;
